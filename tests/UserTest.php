8fcd580feac3c6c2e3e74e1c9304b28def51a61c
skys215 <skys215@gmail.com>
2015-08-07 16:15:13 +0800

    Added register test.

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -5,6 +5,30 @@ class UserTest extends TestCase
 
     public $type = 'android';
 
+    public function testUserRegister(){
+
+        $data = array(
+            'type'       => 'mobile',
+            'code'       => 'int',
+            'mobile'     => "15018749411",
+            'password'   => '123123',
+            'nickname'   => 'nickname',
+            'avatar'     => 'http://7u2spr.com1.z0.glb.clouddn.com/20150605-15425755715301a7625.jpg',
+            'location'   =>  '',
+            'city'       =>  10,
+            'province'   =>  32,
+            'sex'        => '0',
+            'openid'     => '',
+            'auth'       =>  '',
+            'avatar_url' =>  ''
+        );
+
+
+        $res = $this->post('/v1/user/save', $data);
+
+        $this->assertEquals( $res->data, '手机已注册');
+    }
+
     public function testUserLogin(){
         $res = $this->get('/v1/user/login');
 
