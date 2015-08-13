28b1d0d3a0e5cec04f205a409f8042f2c5f65fd3
skys215 <skys215@gmail.com>
2015-08-13 15:00:18 +0800

    Added check token validity.

--- app/Http/Controllers/Android/AccountController.php
+++ app/Http/Controllers/Android/AccountController.php
@@ -75,6 +75,7 @@ class AccountController extends ControllerBase{
     }
 
 
+    //public function getAuthCodeAction(){
     public function requestAuthCodeAction(){
         $phone = $this->get('phone','mobile',0);
         if( !$phone ){
@@ -122,90 +123,25 @@ class AccountController extends ControllerBase{
         return $this->output( ['status'=>(bool)$result] );
     }
 
+    public function hasRegisteredAction(){
+        $phone = $this->get( 'phone', 'mobile', '' );
+        if( !$phone ){
+            return error( 'WRONG_ARGUMENTS', '手机号格式错误' );
+        }
 
+        $hasRegistered = sUser::checkHasRegistered( 'mobile', $phone );
 
-
-
-
-
-
-
-
-
-
-
-
-
-    public function device_tokenAction() {
-        $uid      = $this->_uid;
-
-        $name     = $this->post("device_name", 'string');
-        $os       = $this->post("device_os", 'string');
-        $platform = $this->post('platform','int', 0);
-        $mac      = $this->post("device_mac", 'string');
-        $token    = $this->post("device_token", 'string');
-        $options  = $this->post("options", 'string', '');
-
-        /*
-        $name = 'm2';
-        $os   = 'android';
-        $platform = 0;
-        $mac = '123';
-        $token = '1234';
-         */
-
-        if( empty($mac) )
-            return error('EMPTY_DEVICE_MAC');
-        if( empty($os) )
-            return error('EMPTY_DEVICE_OS');
-        if( empty($token) )
-            return error('EMPTY_DEVICE_TOKEN');
-
-        $deviceInfo = sDevice::updateDevice( $name, $os, $platform, $mac, $token, $options );
-        $userDevice = sUserDevice::bindDevice( $uid, $deviceInfo->id );
-
-        return $this->output();
+        return $this->output( [ 'has_registered' => $hasRegistered ] );
     }
 
+    public function checkTokenValidityAction(){
+        $token = $this->post( 'token', 'string' );
 
-    /**
-     * 检测手机是否被注册
-     */
-    public function check_mobileAction() {
-        $phone = $this->get('phone', 'string', '');
-        if( empty($phone) ){
-            return error('WRONG_ARGUMENTS');
+        if( !$token ){
+            return error( 'WRONG_ARGUMENTS' );
         }
 
-        if (!match_phone_format($phone)) {
-            return error('INVALID_PHONE_NUMBER');
-        }
-        if ( sUser::getUserByPhone($phone) )  {
-            return error('PHONE_ALREADY_EXIST', 'phone already exist', array(
-                'is_register' => 1
-            ));
-        }
-
-        return $this->output(array(
-            'is_register'=>0
-        ));
-    }
-
-
-
-    /**
-     * 检查token是否有效
-     */
-    public function check_tokenAction()
-    {
-        $token = $this->post('token','string');
-        if(!$token || $token == '') {
-            return $this->output( 'err' );
-        }
-
-        if($this->check_token($token)) {
-            return $this->output();
-        }
-        return $this->output( 'err' );
+        $isValid = $this->check_token();
+        return $this->ouput( [ 'is_valid' => $isValid ] );
     }
 }
