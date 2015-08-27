cab204897edd9b56bec3c881d17562d9501299b4
Valentino <valentino@163.com>
2015-08-27 17:47:48 +0800

    style

--- resources/views/main/user/home.blade.php
+++ resources/views/main/user/home.blade.php
@@ -4,7 +4,7 @@
 <div class="inner-container">
 	<div class="personage-container">
 	<!-- 个人页面的左侧 -->
-		<div class="personage-message">
+		<div class="personage-message left">
 			<div class="personage-header">
 				<span class="home-page-icon bg-sprite"></span>
 				<span class="return-home-page">返回首页</span>
@@ -36,14 +36,14 @@
 			</div>
 			<!-- 个人页面nav -->
 			<div class="personage-nav">
-				<span class="personage-special-column">
+				<!-- <span class="personage-special-column">
 					<span class="border-nav"></span>
 					<span class="special-column-icon bg-sprite left"></span>
 					<span class="special-column left">专栏</span>
 					<span class="special-column-count right">123</span>
-				</span>
+				</span> -->
 				<span class="personage-seek-help">
-					<span></span>
+					<span class="border-nav"></span>
 					<span class="seek-help-icon bg-sprite left"></span>
 					<span class="seek-help left">求P</span>
 					<span class="seek-help-count right">123</span>
@@ -60,6 +60,159 @@
 					<span class="under-way left">进行中</span>
 					<span class="under-way-count right">123</span>
 				</span>
+			</div>
+		</div>
+		<!-- 个人页面右边 进行中 图片详情 -->
+		<div class="under-way-container left">
+			<!-- section -->
+			<div class="photo-item left">
+				<div class="photo-item-content">
+					<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg" alt="">
+					<div class="photo-item-reply">
+		                <div class="photo-item-reply-work">
+		                    <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		                </div>
+           			 </div>
+				</div>
+				<div class="photo-item-personage">
+		            <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		            <span class="photo-item-author">
+		                陈晨
+		            </span>
+		            <span class="photo-item-created">
+		                2小时前
+		            </span>
+		         </div>
+		         <div class="uploading-delete">
+		         	<span class="uploading-icon bg-sprite left"></span>
+		         	<span class="uploading left">上传作品</span>
+		         	<span class="delete right">删除</span>
+		         </div>
+			</div>
+			<!-- section -->
+			<div class="photo-item left">
+				<div class="photo-item-content">
+					<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg" alt="">
+					<div class="photo-item-reply">
+		                <div class="photo-item-reply-work">
+		                    <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		                </div>
+           			 </div>
+				</div>
+				<div class="photo-item-personage">
+		            <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		            <span class="photo-item-author">
+		                陈晨
+		            </span>
+		            <span class="photo-item-created">
+		                2小时前
+		            </span>
+		         </div>
+		         <div class="uploading-delete">
+		         	<span class="uploading-icon bg-sprite left"></span>
+		         	<span class="uploading left">上传作品</span>
+		         	<span class="delete right">删除</span>
+		         </div>
+			</div>
+			<!-- section -->
+			<div class="photo-item left">
+				<div class="photo-item-content">
+					<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg" alt="">
+					<div class="photo-item-reply">
+		                <div class="photo-item-reply-work">
+		                    <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		                </div>
+           			 </div>
+				</div>
+				<div class="photo-item-personage">
+		            <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		            <span class="photo-item-author">
+		                陈晨
+		            </span>
+		            <span class="photo-item-created">
+		                2小时前
+		            </span>
+		         </div>
+		         <div class="uploading-delete">
+		         	<span class="uploading-icon bg-sprite left"></span>
+		         	<span class="uploading left">上传作品</span>
+		         	<span class="delete right">删除</span>
+		         </div>
+			</div>
+			<!-- section -->
+			<div class="photo-item left">
+				<div class="photo-item-content">
+					<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg" alt="">
+					<div class="photo-item-reply">
+		                <div class="photo-item-reply-work">
+		                    <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		                </div>
+           			 </div>
+				</div>
+				<div class="photo-item-personage">
+		            <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		            <span class="photo-item-author">
+		                陈晨
+		            </span>
+		            <span class="photo-item-created">
+		                2小时前
+		            </span>
+		         </div>
+		         <div class="uploading-delete">
+		         	<span class="uploading-icon bg-sprite left"></span>
+		         	<span class="uploading left">上传作品</span>
+		         	<span class="delete right">删除</span>
+		         </div>
+			</div>
+			<!-- section -->
+			<div class="photo-item left">
+				<div class="photo-item-content">
+					<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg" alt="">
+					<div class="photo-item-reply">
+		                <div class="photo-item-reply-work">
+		                    <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		                </div>
+           			 </div>
+				</div>
+				<div class="photo-item-personage">
+		            <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		            <span class="photo-item-author">
+		                陈晨
+		            </span>
+		            <span class="photo-item-created">
+		                2小时前
+		            </span>
+		         </div>
+		         <div class="uploading-delete">
+		         	<span class="uploading-icon bg-sprite left"></span>
+		         	<span class="uploading left">上传作品</span>
+		         	<span class="delete right">删除</span>
+		         </div>
+			</div>
+			<!-- section -->
+			<div class="photo-item left">
+				<div class="photo-item-content">
+					<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg" alt="">
+					<div class="photo-item-reply">
+		                <div class="photo-item-reply-work">
+		                    <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		                </div>
+           			 </div>
+				</div>
+				<div class="photo-item-personage">
+		            <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15144955b72be936de0.jpg">
+		            <span class="photo-item-author">
+		                陈晨
+		            </span>
+		            <span class="photo-item-created">
+		                2小时前
+		            </span>
+		         </div>
+		         <div class="uploading-delete">
+		         	<span class="uploading-icon bg-sprite left"></span>
+		         	<span class="uploading left">上传作品</span>
+		         	<span class="delete right">删除</span>
+		         </div>
 			</div>
 		</div>
 	</div>
