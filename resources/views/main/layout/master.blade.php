156f5c3c2891db21b9411ac7b03a0b991aa8f8af
Valentino <valentino@163.com>
2015-09-18 15:14:13 +0800

    style

--- resources/views/main/layout/master.blade.php
+++ resources/views/main/layout/master.blade.php
@@ -40,11 +40,11 @@
                     <a class="menu-bar-item">排行榜</a> 
                     -->
                     <a class="menu-bar-item-last" href="/app/download">app下载</a>
-                    <a class="menu-bar-upload-btn"  href="/user/home">上传作品</a>
                 </div>
             </div>
 
                 <div class="right setting">
+                    <a class="menu-bar-upload-btn"  href="/user/home">上传作品</a>
                     @if (isset($_uid))
                     <span class="user-avatar">
                         <img src="http://7u2spr.com1.z0.glb.clouddn.com/20150728-15205355b72d5512571.jpg">
