d0153f06a030a42769c474ee4e716141d65b9ce7
junqiang <whenjonny@gmail.com>
2015-09-25 21:17:48 +0800

    main

--- public/scripts/app/views/LoginView.js
+++ public/scripts/app/views/LoginView.js
@@ -8,6 +8,31 @@ define(['app/views/Base', 'app/models/User', 'tpl!app/templates/LoginView.html']
             template: template,
             construct: function () {
                 var self = this;
-            }
+            },
+            login: function(e) {
+                var self = this;
+                var username = $('#login_name').val();
+                var password = $('#login_password').val();
+                
+                if (username == '') {
+                    alert('!');   
+                    return false;
+                } 
+                if (password == '') {
+                    alert('?');    
+                    return false;
+                }
+                var user = new User;
+                user.url  = "/login";
+                user.data = {
+                    'username': username,
+                    'password': password
+                };
+                user.fetch({success:function(){ 
+                    location.href = '#asks';
+                    //登录成功之后刷新页面
+                    //window.location.reload();
+                }});
+            },
         });
     });
