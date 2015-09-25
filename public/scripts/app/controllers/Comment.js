afb75205ff03e03fac0f6f53d873a09672a31365
Valentino <valentino@163.com>
2015-09-25 19:18:39 +0800

    style

---
+++ public/scripts/app/controllers/Comment.js
@@ -0,0 +1,9 @@
+define(['underscore', 'app/views/CommentView'],
+    function (_,  CommentView) {
+        "use strict";
+
+        return function(id) {
+
+            var view = new CommentView({});
+        };
+    });
--- public/scripts/app/views/LoginView.js
+++ public/scripts/app/views/LoginView.js
@@ -4,8 +4,41 @@ define(['app/views/Base', 'tpl!app/templates/LoginView.html'],
         
         return View.extend({
             template: template,
+            events: {
+            	"click #login_btn" : "login",
+            },
             construct: function () {
                 var self = this;
+            },
+            login: function() {
+        	  /**
+			    * 登录代码
+			    * @author brandwang
+			    */
+			    $('#login_btn').click(function() {
+			        var username = $('#login_name').val();
+			        var password = $('#login_password').val();
+			        
+			        if (username == '') {
+			            alert('!');   
+			        } else if (password == '') {
+			            alert('?');    
+			        } else {
+			            var url  = "/user/login";
+			            var data = {
+			                'username': username,
+			                'password': password
+			            };
+			            psAjax(url, 'POST', data, function(data) {
+			                var loginModal = $('[data-remodal-id=login-modal]').remodal();
+			                if (loginModal.getState() == 'opened') {
+			                    loginModal.close();    
+			                }
+			                //登录成功之后刷新页面
+			                window.location.reload();
+			            });
+			        }
+			    }); 
             }
         });
     });
