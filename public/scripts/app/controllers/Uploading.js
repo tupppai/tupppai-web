f73a1f54d5cd51f57c1567ba98a61eda858421ae
Valentino <valentino@163.com>
2015-10-06 15:45:02 +0800

    style

--- public/scripts/app/controllers/Uploading.js
+++ public/scripts/app/controllers/Uploading.js
@@ -4,12 +4,5 @@ define(['app/views/UploadingView'],
 
         return function() {
 
-            var view = new UploadingView().render();
-            console.log(view);
-            var html = view.el;
-
-            $("#modalView").html(html);
-            $('div[data-remodal-id=uploading-modal]').remodal().open();
-        };
-
+        }
     });
