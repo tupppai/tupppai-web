9101c4839316a1a1c663a535841e6bc0252f5494
Valentino <valentino@163.com>
2015-10-10 11:35:18 +0800

    style

--- resources/views/admin/verify/test.php
+++ resources/views/admin/verify/test.php
@@ -1,1 +1,30 @@
-test
+<link rel="stylesheet" href="/main/css/thread_admin.css" type="text/css" >
+<div class="admin-card-container"> 
+	<div class="card-header">
+		<span class="ask-id">
+			原图ID:<i>123</i>
+		</span>
+		<span class="ask-picture-check">
+			<input type="checkbox">
+		</span>
+	</div>
+	<div class="ask-picture">
+		<img src="http://7u2spr.com1.z0.glb.clouddn.com/20150402-153128551cf0501f1d0.jpg?imageView2/2/w/300" alt="">
+	</div>
+	<div class="ask-describe">
+		我要帅气
+	</div>
+	<div class="actionbar-btn">
+		<button class="">标签</button>
+		<button class="">创意</button>
+		<button class="">精修</button>
+	</div>
+	<div class="waistcoat-message">
+		<span class="waistcoat-user">马甲号</span>
+		<option class="waistcoat-option" value="">头像/昵称/用户</option>
+	</div>
+	<div class="timing-effect">
+		<span class="timing-issue">定时发布</span>	
+		<option class="timing" value="">2015-12-12 12:05:12</option>
+	</div>
+</div>
