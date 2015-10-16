41e8f38ee23dab519dbcaa2827876530cdb9989b
skys215 <skys215@qq.com>
2015-10-16 10:33:13 +0800

    Added select all for reviewAsk.

--- resources/views/admin/reviewAsk/wait.php
+++ resources/views/admin/reviewAsk/wait.php
@@ -54,7 +54,9 @@
           已发布</a>
       </li>
 </div>
-
+<label for="selectAll">
+    <input type="checkbox" name="selectAll" id="selectAll" checked="checked"/>全选
+</label>
 <ul id="review-data"></ul>
 
 <?php modal('/review/review_item'); ?>
@@ -144,6 +146,17 @@ jQuery(document).ready(function() {
               location.reload();
             }
         });
+    });
+
+    $('#selectAll').on('click', function(){
+        var all = $(this).prop('checked');
+        var checkboxes = $('.admin-card-container input[name="confirm_online"]');
+        if( all ){
+            checkboxes.prop('checked', 'checked');
+        }
+        else{
+            checkboxes.removeProp('checked');
+        }
     });
 
 });
