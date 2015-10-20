255a2717e5de6c866009cf1192712f680e6b2ce9
skys215 <skys215@gmail.com>
2015-10-20 20:19:25 +0800

    Added checkbox and tab for populars

--- resources/views/admin/verify/categories.php
+++ resources/views/admin/verify/categories.php
@@ -7,6 +7,21 @@
   </li>
   <li>多图审核</li>
 </ul>
+
+<div class="tabbable-line">
+    <ul class="nav nav-tabs">
+      <li class="all active">
+        <a href="/verify/categories">审核库</a>
+      </li>
+      <li class="hot">
+        <a href="/verify/categories?type=pending">热门</a>
+      </li>
+      <li class="pc_hot">
+        <a href="/verify/categories?type=sent">PC热门</a>
+      </li>
+    </ul>
+</div>
+
 <div class="form-inline">
     <div class="form-group">
         <input name="uid" class="form-filter form-control" placeholder="账号ID">
@@ -21,13 +36,24 @@
         <button type="submit" class="form-filter form-control" id="search" >搜索</button>
     </div>
 </div>
-
+<label for="selectAll">
+    <input type="checkbox" name="selectAll" id="selectAll" checked="checked"/>全选
+</label>
 <div id="thread-data"></div>
 
 <?php modal('/verify/check_item'); ?>
 <?php modal('/verify/reply_comment'); ?>
 
-
+<button class="btn btn-danger delete" style="width: 20%">拒绝</button>
+<button class="btn btn-success update" style="width: 20%">确定</button>
+<style>
+    .photo-container-admin{
+        border: 1px solid transparent;
+    }
+    .photo-container-admin:hover{
+        border: 1px solid steelblue;
+    }
+</style>
 <script>
 var table = null;
 jQuery(document).ready(function() {
@@ -44,28 +70,89 @@ jQuery(document).ready(function() {
 
 $(document).ready(function(){
     $('#thread-data').on('click', '.chg_stat', function(){
-        var par = $(this).parents('div.photo-container-admin');
-        var target_type = par.attr('data-target-type');
-        var target_id = par.attr('data-target-id');
-        //var status = Number($(this).attr('data-status')) > 0 ? -1 : 1;
-        var status = 1;//通过的状态
-        var data = {
-            'target_id': target_id,
-            'target_type': target_type,
-            'status' : status
-        };
-
-        $.post( '/verify/set_thread_status', data, function( data ){
-            data=data.data;
-            if( data.result == 'ok' ){
-                table.submitFilter();
-            }
-        } );
-
+        var t = $(this).siblings('span.btn_text');
+        var c = $(this);
+        var txt = t.text();
+        var cancel_text = '取消';
+        if( c.prop('checked') == true ){
+            txt = cancel_text + txt;
+        }
+        else{
+            txt = txt.substr( 2 );
+        }
+        t.text( txt );
     });
 
     $('#thread-data').on('click', '.categorize', function(){
 
     });
+
+
+
+
+
+    $('.online').on('click', function(){
+        if( !updateInfo() ){
+            return false;
+        }
+        var ids = [];
+        var hasFault = false;
+        $('.admin-card-container').removeClass('wrong');
+
+        $('.admin-card-container input[name="confirm_online"]:checked').each(function(i,n){
+            var cont = $(this).parents('.admin-card-container');
+            ids.push( cont.attr('data-id') );
+            var release_time= Date.parse( cont.find('input[name="release_time"]').val() )/1000;
+            if( release_time < Math.ceil( (new Date()).getTime() / 1000 ) ){
+                cont.addClass('wrong');
+                hasFault = true;
+            }
+        });
+        if( hasFault ){
+            toastr['warning']('发布时间不能是过去的时间');
+            return;
+        }
+
+        var postData = {
+            'review_ids': ids,
+            'status': -1
+        };
+
+        $.post('/review/set_status', postData, function( data ){
+            if( data.data.result == 'ok' ){
+                location.reload();
+            }
+        });
+        return true;
+    });
+
+    $('.update').on('click', function(){
+        updateInfo()
+    });
+
+    $('.delete').on('click', function(){
+        var ids = [];
+        $('.photo-container-admin input[name="confirm_online"]:checked').each(function(i,n){
+            ids.push( $(this).parents('.photo-container-admin').attr('data-id') );
+        });
+        $.post('/review/set_status', {'review_ids': ids, 'status': 0}, function( data ){
+            if( data.data.result == 'ok' ){
+              location.reload();
+            }
+        });
+    });
+
+    $('#selectAll').on('click', function(){
+        var all = $(this).prop('checked');
+        var checkboxes = $('.photo-container-admin input[name="confirm_online"]');
+        if( all ){
+            checkboxes.prop('checked', 'checked');
+        }
+        else{
+            checkboxes.removeProp('checked');
+        }
+    });
+
+
 });
 </script>
