8aca2acd01b5dcd74412ae70872048ba5c348744
skys215 <skys215@gmail.com>
2015-10-24 15:04:50 +0800

    Added uid for count update

---
+++ app/Jobs/UpReply.php
@@ -0,0 +1,40 @@
+<?php namespace App\Jobs;
+
+use Illuminate\Contracts\Bus\SelfHandling;
+
+use App\Services\Reply as sReply;
+use App\Models\Reply as mReply;
+
+
+class UpReply extends Job
+{
+    public $condition   = array();
+
+    public $sender_uid;
+    public $target_id;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct( $target_id, $sender_uid  )
+    {
+        $this->sender_uid  = $sender_uid;
+        $this->target_id   = $target_id;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if( $this->attempts() > 3 ){
+            $this->delete();
+        }
+        $ret = sReply::upReply( $this->target_id, $this->sender_uid, mReply::STATUS_NORMAL );
+    }
+
+
+}
