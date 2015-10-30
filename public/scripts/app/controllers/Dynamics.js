9049bf91abb80bcebe1561b4403db988e7214bb0
Valentino <valentino@163.com>
2015-10-30 11:04:05 +0800

    setting

---
+++ public/scripts/app/controllers/Dynamics.js
@@ -0,0 +1,11 @@
+define(['underscore', 'app/views/DownloadView'],
+    function (_, DownloadView) {
+        "use strict";
+
+        return function() {
+            var view = new DownloadView();
+
+            window.app.home.close();
+            window.app.content.show(view);
+        };
+    });
