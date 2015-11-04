704be6636cbc5038aa1bdc8bc6340db65685b472
junqiang <whenjonny@gmail.com>
2015-11-04 15:31:24 +0800

    pc followlist

--- public/scripts/app/controllers/Friendship.js
+++ public/scripts/app/controllers/Friendship.js
@@ -6,10 +6,19 @@ define([
     function (_, FriendshipView, Friendships) {
         "use strict";
 
-        return function() {
+        return function(type, uid) {
         	var friendships = new Friendships;
-            friendships.fetch();
+            if(type == 'follows') {
+                friendships.url = '/follows';
+            }
+            else {
+                friendships.url = '/fans';
+            }
+            friendships.data.uid = uid;
             var view = new FriendshipView({collection: friendships});
+
+            $(window.app.content.el).attr('data-type', type);
+            $(window.app.content.el).attr('data-uid', uid);
 
             window.app.content.show(view);
 
