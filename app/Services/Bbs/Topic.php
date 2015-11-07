a05e76b2ad41d1be8609bfe4bdd2eb5d2c8a01e3
nunqiang <whenjonny@gmail.com>
2015-11-07 15:18:20 +0800

    search add user info

--- app/Services/Bbs/Topic.php
+++ app/Services/Bbs/Topic.php
@@ -1,11 +1,30 @@
 <?php namespace App\Services\Bbs;
 
 use App\Services\ServiceBase;
+
 use App\Models\Bbs\Topic as mTopic;
+
+use App\Services\User as sUser;
+
 use App\Facades\CloudCDN;
 
 class Topic extends ServiceBase{
     public static function searchTopics($keyword, $page, $size) {
-        return (new mTopic)->search_topics($keyword, $page, $size);
+        $topics = (new mTopic)->search_topics($keyword, $page, $size);
+
+        $data = array();
+        foreach($topics as $topic) {
+            $data = self::brief($topic);
+
+            $user = sUser::getUserByUid($topic->uid);
+            $data = array_merge($data, sUser::brief($user));
+        }
+        return $data;
+    }
+
+    public static function brief ( $data ) {
+        $data = $data->toArray();
+
+        return $data;
     }
 }
