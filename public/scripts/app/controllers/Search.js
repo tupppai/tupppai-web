b713e93cd91189866ac8e79a034483de0f5a517d
Valentino <valentino@163.com>
2015-11-07 11:30:08 +0800

    style

--- public/scripts/app/controllers/Search.js
+++ public/scripts/app/controllers/Search.js
@@ -1,10 +1,38 @@
-define(['underscore', 'app/views/search/SearchView'],
-    function (_, SearchView) {
+define([
+        'underscore', 
+        'app/collections/Friendships',
+        'app/collections/Replies',
+        'app/views/search/SearchView',
+        'app/views/search/UserItemView',
+        'app/views/search/ContentItemView',
+        'app/views/search/DiscussItemView',
+       ],
+    function (_, Friendships, Replies, SearchView, UserItemView, ContentItemView, DiscussItemView) {
         "use strict";
 
         return function() {
-            var view = new SearchView();
+            var friendships = new Friendships;
+            var reply = new Replies;
 
+            var view = new SearchView();
             window.app.content.show(view);
+    
+            var userRegion = new Backbone.Marionette.Region({el:"#userItemView"});
+            var view = new UserItemView({
+                collections: friendships
+            });
+            userRegion.show(view);
+    
+    
+            var contentRegion = new Backbone.Marionette.Region({el:"#contentItemView"});
+            var view = new ContentItemView({
+                collections: reply
+            });
+            contentRegion.show(view);
+
+
+            var discussRegion = new Backbone.Marionette.Region({el:"#discussItemView"});
+            var view = new DiscussItemView({});
+            discussRegion.show(view);
         };
     });
