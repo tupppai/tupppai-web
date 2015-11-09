fcf28c17d982bcfe3308d602aa55d2c9ab68f45f
tuppai <tuppai@tuppaideMac-mini.local>
2015-11-09 18:36:35 +0800

    Done admin inform.

--- app/Services/Inform.php
+++ app/Services/Inform.php
@@ -6,7 +6,8 @@ use App\Services\ActionLog  as sActionLog;
 use App\Services\User       as sUser;
 use App\Services\Ask        as sAsk;
 use App\Services\Reply      as sReply;
-use App\Services\Comment      as sComment;
+use App\Services\Comment    as sComment;
+use App\Services\Usermeta   as sUsermeta;
 
 use App\Models\Inform  as mInform;
 use App\Models\User    as mUser;
@@ -112,26 +113,128 @@ class Inform extends ServiceBase {
 		return self::brief($report);
 	}
 
-	public static function dealReport( $id, $uid, $result, $status = mInform::INFORM_STATUS_SOLVED ){
-		$mInform = new mInform();
+	public static function dealReport( $id, $uid, $type ){
+		$report = (new mInform)->get_inform_by_id( $id );
+		if( !$report ){
+			return error( 'INFORM_NOT_EXIST', '举报记录不存在');
+		}
+		if( $report->status != mInform::INFORM_STATUS_PENDING ){
+			return '该举报已被处理。';
+		}
 
-		$report = $mInform->get_inform_by_id( $id );
-		if( $report->status != $this::INFORM_STATUS_PENDING ){
+		switch( $type ){
+			case 'block_reporter':
+				$ret = self::block_reporter($report->uid);
+				if( $ret ){
+					sActionLog::init('FORBID_USER');
+					sActionLog::log( $ret );
+				}
+				$content = '已对举报者实施禁言。';
+				$status = mInform::INFORM_STATUS_SOLVED;
+				break;
+			case 'block_author':
+				$ret = self::block_author($report);
+				if( $ret ){
+					//获取以前的禁言设置？
+					//如果用户不存在……
+					sActionLog::init( 'FORBID_USER' );
+					sActionLog::log( $ret );
+				}
+				$content = '已对被举报对象作者实施禁言。';
+				$status = mInform::INFORM_STATUS_SOLVED;
+				break;
+			case 'ignore':
+				$ret = true;//$this->ignore_report($report);
+				$content = '已忽略该举报。';
+				$status = mInform::INFORM_STATUS_IGNORED;
+				break;
+			case 'false_report':
+				$ret = true;//$this->false_report($report);
+				$content = '已标记该举报为误报。';
+				$status = mInform::INFORM_STATUS_IGNORED;
+				break;
+			default:
+				return $this->error( 'TYPE_NOT_EXIST', '不存在的处理类型' );
+				break;
+		}
+		if( $ret ){
+			$res = $report->deal_report($report->id, $uid, $content, $status);
+			if( $res ){
+				sActionLog::init( 'DEAL_INFORM', $report );
+				sActionLog::log( $res );
+			}else{
+				$content = '处理失败';
+			}
+		}
+		else{
+			$content = '处理失败';
+		}
+		return $content;
+	}
+
+
+	public static function block_reporter($uid){
+		$value = 60/*sec*/ * 60/*min*/ * 24/*hours*/ * 7/*days*/;
+
+		if(!$uid) {
+			return error( 'EMPTY_UID', '用户不存在');
+		}
+		$user = sUser::getUserByUid($uid);
+		if(!$user) {
+			return error( '', '用户不存在');
+		}
+
+		return sUsermeta::write_user_forbid($uid, $value);
+	}
+
+	public static function block_author($report){
+		$value = 60/*sec*/ * 60/*min*/ * 24/*hours*/ * 7/*days*/;
+
+		if( !$report ){
 			return false;
 		}
-        sActionLog::init('DEAL_INFORM', $report);
 
-		$report ->assign(array(
-			'status'      => $status,
-			'oper_time'   => time(),
-			'oper_by'     => $uid,
-			'oper_result' => $result
-		));
+		$target_type = $report->target_type;
+		$target_id = $report->target_id;
 
-		$report->save();
-        sActionLog::save( $report);
-		return self::brief($report);
-    }
+		switch( $target_type ){
+			case mInform::TYPE_ASK:
+				$ask = sAsk::getAskById( $target_id );
+				if( !$ask ){
+					return false;
+				}
+				$uid = $ask->uid;
+				break;
+			case mInform::TYPE_REPLY:
+				$reply = sReply::getReplyById( $target_id );
+				if( !$reply ){
+					return false;
+				}
+				$uid = $reply->uid;
+				break;
+			case mInform::TYPE_COMMENT:
+				$comment = sComment::getCommentById( $target_id );
+				if( !$comment ){
+					return false;
+				}
+				$uid = $comment->uid;
+				break;
+			case mInform::TYPE_USER:
+				$uid = $target_id;
+				break;
+		}
+
+
+		if(!$uid) {
+			return error( '用户不存在' );
+		}
+		$user = sUser::getUserByUid($uid);
+		if(!$user) {
+			return error( '用户不存在' );
+		}
+
+		return sUsermeta::write_user_forbid($uid, $value);
+	}
 
 	public static function brief( $informObj ){
 		$obj = array(
@@ -139,5 +242,9 @@ class Inform extends ServiceBase {
 			'create_time' => $informObj->create_time
 		);
 		return $obj;
+    }
+
+    public static function getInformById( $id ){
+		return (new mInform)->get_inform_by_id( $id );
     }
 }
