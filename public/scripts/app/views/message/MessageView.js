11f9f96e69d9226ee18b7f2e5c6bab087821179b
Valentino <valentino@163.com>
2015-11-09 15:26:41 +0800

    message

--- public/scripts/app/views/message/MessageView.js
+++ public/scripts/app/views/message/MessageView.js
@@ -1,37 +1,17 @@
-define([
-        'app/views/Base', 
-        'tpl!app/templates/message/MessageView.html', 
-        'tpl!app/templates/message/MessageItemView.html'
-        ],
-    function (View, template, itemTemplate) {
+define(['app/views/Base', 'tpl!app/templates/message/MessageView.html'],
+         
+    function (View, template) {
         "use strict";
         
         return View.extend({
             tagName: 'div',
             className: '',
             template: template,
-            itemTemplate: itemTemplate,
             events: {
-                'click .message .nav' : 'switchNav'
+                'click .message .nav' : 'switchNav',
             },
             construct: function() {
-                var self = this;
                 $("a.menu-bar-item").removeClass('active');
-
-                this.listenTo(this.collection, "change", this.renderList);
-
-                self.scroll();
-                self.collection.loading(self.showEmptyView);
-            },
-            renderList: function() {
-                var template = this.itemTemplate;
-                var el       = $('#message-item-list');
-
-                this.collection.each(function(model){
-                    var html = template(model.toJSON());
-                    el.append(html);
-                });
-                this.onRender();
             },
             switchNav: function(e) {
                 var self = this;
