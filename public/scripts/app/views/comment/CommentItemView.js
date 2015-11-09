7280dc37cc404555789db0aa030be3183ad54c51
nunqiang <whenjonny@gmail.com>
2015-11-09 14:28:03 +0800

    view update

--- public/scripts/app/views/comment/CommentItemView.js
+++ public/scripts/app/views/comment/CommentItemView.js
@@ -11,10 +11,10 @@ define(['app/views/Base',   'tpl!app/templates/comment/CommentItemView.html'],
             },
             // 求助图片切换
             photoShift: function(e) {
-                     var AskSmallUrl = $(e.currentTarget).find('img').attr("src");
-                     var AskLargerUrl = $(e.currentTarget).prev().find('img').attr("src");
-                     $(e.currentTarget).prev().find('img').attr("src",AskSmallUrl);
-                     $(e.currentTarget).find('img').attr("src",AskLargerUrl);              
+                 var AskSmallUrl = $(e.currentTarget).find('img').attr("src");
+                 var AskLargerUrl = $(e.currentTarget).prev().find('img').attr("src");
+                 $(e.currentTarget).prev().find('img').attr("src",AskSmallUrl);
+                 $(e.currentTarget).find('img').attr("src",AskLargerUrl);              
             },
             construct: function() {
 				var self = this;
