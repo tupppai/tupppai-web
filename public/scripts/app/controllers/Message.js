b5e0e80276e82bdd062738688fd4e95c138c6c54
nunqiang <whenjonny@gmail.com>
2015-11-09 15:43:13 +0800

    message

--- public/scripts/app/controllers/Message.js
+++ public/scripts/app/controllers/Message.js
@@ -1,15 +1,25 @@
 define([
+        'app/models/Message',
+        'app/collections/Messages',
         'app/views/message/MessageView', 
+        'app/views/message/MessageItemView'
 	   ],
-    function ( MessageView) {
+    function ( Message, Messages, MessageView, messageItemView) {
         "use strict";
 
-        return function() {
-            // var messages = new Messages;
-            // messages.data.type = 'comment';
+        return function(type, uid) {
+            var messages = new Messages;
+            if(!type) type = 'comment';
+            messages.data.type = type;
 
-            var view = new MessageView({});
-
+            var message = new Message({type: type});
+            var view = new MessageView({model: message});
             window.app.content.show(view);
+
+            var messageListRegion = new Backbone.Marionette.Region({el:"#message-item-list"});
+            var view = new messageItemView({
+                collection: messages 
+            });
+            messageListRegion.show(view);
         };
     });
