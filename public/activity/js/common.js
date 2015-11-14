ab9c707c01329448bbb80d35e416215f16640f9d
nunqiang <whenjonny@gmail.com>
2015-11-14 17:30:14 +0800

    cookie

--- public/activity/js/common.js
+++ public/activity/js/common.js
@@ -3,5 +3,3 @@ function _get(name) {
     var r = window.location.search.substr(1).match(reg);
     if (r != null) return unescape(r[2]); return null;
 }
-
-$.fn.cookie('token', _get('token'), { path: '/', expires: 30, http: true });
