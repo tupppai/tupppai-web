221a038873e2cd8f1c143ed9acd116903d63feed
junqiang <whenjonny@gmail.com>
2015-11-17 13:14:05 +0800

    tower push

--- app/Http/Controllers/Admin/PushController.php
+++ app/Http/Controllers/Admin/PushController.php
@@ -3,6 +3,7 @@
 use Event, Log;
 
 use App\Services\ActionLog as sActionLog;
+use App\Models\ActionLog as mActionLog;
 
 class PushController extends ControllerBase{
 
@@ -10,10 +11,37 @@ class PushController extends ControllerBase{
         'tower',
     );
 
+    public function indexAction() {
+        return $this->output();
+    }
+
+    public function list_pushesAction(){
+        $model = new mActionLog('action_task');
+
+        $cond = array();
+        $cond['uid'] = array(
+            $this->post('uid'),
+            'LIKE',
+            'AND'
+        );
+        $cond['status'] = mActionLog::STATUS_NORMAL;
+        $join = array();
+        $order = array();
+
+        $data = $this->page($model, $cond, $join, $order );
+        foreach ($data['data'] as $app) {
+            $app->update_time = date('Y-m-d H:i:s', $app->update_time);
+            $app->create_time = date('Y-m-d H:i:s', $app->create_time);
+        }
+
+        return $this->output_table($data);
+    }
+
     public function towerAction() {
         $request_body = file_get_contents('php://input');
 
         sActionLog::addTowerTaskAction($request_body);
+        Log::info('tower', array($request_body));
     }
 
 }
