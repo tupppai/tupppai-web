ecd65508fc5110c5c3d10fb39d31a06e5893eace
nunqiang <whenjonny@gmail.com>
2015-11-19 12:54:16 +0800

    auth

--- app/Http/Middleware/AdminAuthMiddleware.php
+++ app/Http/Middleware/AdminAuthMiddleware.php
@@ -34,7 +34,7 @@ class AdminAuthMiddleware {
         session(['user'=>$this->user]); 
 
         //if (!in_array($this->_uid, array(mUserRole::SUPER_USER_UID , mUserRole::STAFF_USER_UID))
-        if (!in_array($this->_uid, mUserRole::staff_uids)
+        if (!in_array($this->_uid, mUserRole::$staff_uids)
             && !sUserRole::checkAuth($this->_uid, mUserRole::ROLE_STAFF) ) {
             return redirect('login');
         }
