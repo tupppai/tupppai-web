3242374f664d0b2de2f943aefcfa0dfb3aac8c09
nunqiang <whenjonny@gmail.com>
2015-11-19 12:46:41 +0800

    admin auth

--- envoy/Envoy.blade.php
+++ envoy/Envoy.blade.php
@@ -1,4 +1,4 @@
-@servers(['web-dev' => 'jq@loiter.us', 'apk-dev' => '127.0.0.1'])
+@servers(['web-dev' => 'jq@loiter.us', 'apk-dev' => '127.0.0.1', 'apk-production' => '127.0.0.1'])
 
 @task('deploy', ['on' => 'web-dev', 'confirm' => false])
     cd /var/www/ps
@@ -7,9 +7,15 @@
     php artisan db:seed
 @endtask
 
-@task('release', ['on' => 'apk-dev', 'confirm' => false])
+@task('package', ['on' => 'apk-dev', 'confirm' => false])
     cd /Users/junqiang/www/psgod-android-as
     git pull origin master
     ./gradlew assembleUmengRelease && curl http://admin.loiter.us/push/mailApk
     scp /Users/junqiang/www/psgod-android-as/appStartActivity/build/outputs/apk/appStartActivity-umeng-release-unsigned.apk jq@loiter.us:/var/www/ps/public/mobile/apk/tupai.apk
+@endtask
+
+@task('release', ['on' => 'apk-production', 'confirm' => false])
+    cd /Users/junqiang/www/psgod-android-as
+    git pull origin master
+    ./gradlew assembleRelease
 @endtask
