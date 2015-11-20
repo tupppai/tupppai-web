7d9f2143368dc040eb6d5f7fff8f7d3e2603b731
skys215 <skys215@gmail.com>
2015-11-20 18:07:08 +0800

    Added total_inform_count for personal index

--- app/Models/Inform.php
+++ app/Models/Inform.php
@@ -52,6 +52,32 @@ class Inform extends ModelBase{
 		return $this->save();
     }
 
+    public function sum_reported_times_by_uid( $uid ){
+        return $this->leftjoin('asks', function( $join ) use ( $uid ){
+                        $join->where( 'informs.target_type', '=', self::TYPE_ASK )
+                             ->on('informs.target_id','=', 'asks.id');
+                    })
+                    ->leftjoin('replies', function( $join ) use ( $uid ){
+                        $join->where( 'informs.target_type', '=', self::TYPE_REPLY )
+                             ->on('informs.target_id', '=', 'replies.id');
+                    })
+                    ->leftjoin('comments', function( $join ) use ( $uid ){
+                        $join->where('informs.target_type','=', self::TYPE_COMMENT )
+                             ->on( 'informs.target_id', '=', 'comments.id');
+                    })
+                    ->where(function($query) use ( $uid ) {
+                        $query =$query->where( 'asks.uid','=', $uid)
+                              ->orwhere( 'replies.uid','=', $uid)
+                              ->orwhere( 'comments.uid','=', $uid);
+                    })
+                    ->whereIn( 'informs.status', [ self::INFORM_STATUS_PENDING, self::INFORM_STATUS_SOLVED ] )
+                    ->count( 'informs.id' );
+    }
+    public function sum_report_times_by_uid( $uid ){
+        return $this->where( 'uid', $uid )
+                    ->whereIn( 'status', [ self::INFORM_STATUS_PENDING, self::INFORM_STATUS_SOLVED ] )
+                    ->count();
+    }
 	//public static function report( $uid, $target_type, $target_id, $content ){
 	//public function deal_report( $id, $uid, $result, $status = Inform::INFORM_STATUS_SOLVED ){
 }
