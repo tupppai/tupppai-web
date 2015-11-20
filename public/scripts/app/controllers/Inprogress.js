9117bc6159b0135df3b330a6b4569460bc63b27c
Valentino <valentino@163.com>
2015-11-20 15:23:59 +0800

    common

--- public/scripts/app/controllers/Inprogress.js
+++ public/scripts/app/controllers/Inprogress.js
@@ -1,11 +1,22 @@
-define(['underscore', 'app/collections/Replies', 'app/views/upload/InprogressView'],
-    function (_, Replies, InprogressView) {
+define(['underscore', 
+		'app/collections/Inprogresses', 
+		'app/views/upload/InprogressItemView',
+		'app/views/upload/InprogressView'
+	],
+    function (_, Inprogresses, InprogressItemView, InprogressView) {
         "use strict";
 
         return function() {
             
-            var view = new InprogressView({});
+            var view = new InprogressView();
 
             window.app.content.show(view);
+
+            var inprogresses = new Inprogresses;
+            var inprogressItemView = new Backbone.Marionette.Region({el:"#InprogressItemView"});
+            var view = new InprogressItemView({
+                collection: inprogresses
+            });
+            inprogressItemView.show(view);
         };
     });
