a7f98f7a923d975891502e6f1af257ae76900617
nunqiang <whenjonny@gmail.com>
2015-11-24 10:53:56 +0800

    将打包流程缩减，暂时只打umeng包

--- daemons/envoy/Envoy.blade.php
+++ daemons/envoy/Envoy.blade.php
@@ -22,7 +22,7 @@
             echo begin build apk
             cd /Users/junqiang/www/tupppai-android
             git pull origin master
-            ./gradlew assembleRelease -Pandroid.injected.signing.store.file=/Users/junqiang/.gradle/keystore -Pandroid.injected.signing.store.password=psgod1234 -Pandroid.injected.signing.key.alias=psgod -Pandroid.injected.signing.key.password=psgod1234
+            ./gradlew assembleUmengRelease -Pandroid.injected.signing.store.file=/Users/junqiang/.gradle/keystore -Pandroid.injected.signing.store.password=psgod1234 -Pandroid.injected.signing.key.alias=psgod -Pandroid.injected.signing.key.password=psgod1234
             #./gradlew assembleUmengRelease && curl http://admin.loiter.us/push/mailApk
             scp /Users/junqiang/www/tupppai-android/appStartActivity/build/outputs/apk/tupppei_v1.0.1_xiaomi.apk jq@loiter.us:/var/www/ps/public/mobile/apk/tupai.apk
             curl http://admin.loiter.us/push/mailApk
