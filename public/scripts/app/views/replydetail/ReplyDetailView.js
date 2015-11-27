e056045d33e837aea959be739ee2b067adb4fe42
Valentino <valentino@163.com>
2015-11-27 20:07:35 +0800

    deploy

--- public/scripts/app/views/replydetail/ReplyDetailView.js
+++ public/scripts/app/views/replydetail/ReplyDetailView.js
@@ -8,7 +8,26 @@ define([
         return View.extend({
             tagName: 'div',
             className: '',
-            template: template
-
+            template: template,
+            events: {
+                "click .written-reply": 'answer'
+            },
+            answer : function(e) {
+                $(e.currentTarget).siblings(".reply-inp").css({
+                    display: "block"
+                }).parents(".comment-content").siblings().children(".reply-inp").css({
+                    display: "none"
+                })
+            },
+            onRender: function() {
+                var timer = setTimeout(function(){
+                    var height= $(".big-pic").height();
+                    console.log($(".big-pic").height())
+                    $(".reply-content").css({
+                        height: $(".big-pic").height() + "px"
+                    })
+                }, 100)
+               
+            }
         });
     });
