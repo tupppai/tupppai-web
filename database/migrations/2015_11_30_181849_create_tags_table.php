38dc5bf589216d3770efae744b3c5e2f4fc5b9c6
nunqiang <whenjonny@gmail.com>
2015-12-01 10:25:21 +0800

    增加tags的数据库

---
+++ database/migrations/2015_11_30_181849_create_tags_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('uid');
+            $table->string('name', 255);
+            $table->integer('user_count', 0);
+            $table->integer('thread_count', 0);
+            $table->integer('status', 0);
+            $table->string('remark', 255);
+            $table->unsignedInteger('release_time');
+            $table->unsignedInteger('create_time');
+            $table->unsignedInteger('update_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tags');
+    }
+}
