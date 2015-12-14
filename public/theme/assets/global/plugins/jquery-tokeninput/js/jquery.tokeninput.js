ed62b8890908e192696aa51c9209cac6e1f3158c
Valentino <valentino@163.com>
2015-12-14 20:28:04 +0800

    deploy

--- public/theme/assets/global/plugins/jquery-tokeninput/js/jquery.tokeninput.js
+++ public/theme/assets/global/plugins/jquery-tokeninput/js/jquery.tokeninput.js
@@ -453,15 +453,17 @@ $.TokenList = function (input, url_or_data, settings) {
           .insertBefore(input_token);
 
         // The 'delete token' button
-        $("<span>" + settings.deleteText + "</span>")
-            .addClass(settings.classes.tokenDelete)
-            .appendTo(this_token)
-            .click(function () {
-                delete_token($(this).parent());
-                hidden_input.change();
-                return false;
-            });
-
+        if( !item.readonly ){
+            $("<span>" + settings.deleteText + "</span>")
+                .addClass(settings.classes.tokenDelete)
+                .appendTo(this_token)
+                .click(function () {
+                    delete_token($(this).parent());
+                    hidden_input.change();
+                    return false;
+                });
+        }
+        
         // Store data on the token
         var token_data = {"id": item.id};
         token_data[settings.propertyToSearch] = item[settings.propertyToSearch];
