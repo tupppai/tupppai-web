c71eb351f71452f8afcb148c07a5f4dac0541856
Valentino <valentino@163.com>
2015-12-14 16:21:36 +0800

    deploy

--- public/scripts/app/views/replydetailplay/ReplyDetailPlayView.js
+++ public/scripts/app/views/replydetailplay/ReplyDetailPlayView.js
@@ -17,10 +17,7 @@ define([
             tagName: 'div',
             className: '',
             template: template,
-           
             events: {
-                // "click .other-click" : "otherPerson",
-                // "click .old-pic" : "oldPerson",
                 "click .like_toggle" : 'likeToggleLarge',
                 "click .center-loading-image-container" : "picScroll",
                 "click #replyDetailRight" : "picScroll",
@@ -248,101 +245,6 @@ define([
                     });
                 }, 700)
             },
-            // otherPerson: function(e) {   
-
-            //     var reply_id = $(e.currentTarget).attr('data-id');
-            //     var type = 2;
-
-            //     $("#replyComment_btn").attr("data-id",reply_id);
-            //     $("#replyComment_btn").attr("data-type", type);
-
-            //     var model = new Reply;
-            //     model.url = '/replies/' + reply_id;
-            //     model.fetch();
-
-            //     var comments = new Comments;
-            //     comments.url = '/comments?target_type=new';
-            //     comments.data.type = type;
-            //     comments.data.target_id = reply_id;
-
-            //     var replyDetailPersonView = new Backbone.Marionette.Region({el:"#replyDetailPersonView"});
-            //     var view = new ReplyDetailPersonView({
-            //         model: model
-            //     });
-            //     replyDetailPersonView.show(view); 
-
-            //     var userIfo = new Backbone.Marionette.Region({el:"#userIfo"});
-            //     var view = new ReplyDetailCommentView({
-            //         collection: comments
-            //     });
-            //     userIfo.show(view); 
-
-            //     var count = new Backbone.Marionette.Region({el:"#count"});
-            //     var view = new ReplyDetailCountView({
-            //         model: model
-            //     });
-            //     count.show(view); 
-
-            //     var action = new Backbone.Marionette.Region({el:"#action"});
-            //     var view = new ReplyDetailActionView({
-            //         model: model
-            //     });
-            //     action.show(view);
-
-            //     setTimeout(function(){
-            //         if($(".reply-comment").height() > 550) {
-            //             $(".reply-more").removeClass("blo");
-            //         } else {
-            //             $(".reply-more").addClass("blo");
-            //         }
-            //         $(".reply-detail-ifo").css({
-            //             overflow: "hidden"
-            //         })
-            //     }, 700)
-            // },
-            // oldPerson: function(e) {   
-
-            //     var reply_id = $(e.currentTarget).attr('data-id');
-            //     var type = 1;
-
-            //     $("#replyComment_btn").attr("data-id",reply_id);
-            //     $("#replyComment_btn").attr("data-type", type);
-
-            //     var model = new Ask;
-            //     model.url = '/asks/' + reply_id;
-            //     model.fetch();                var comments = new Comments;
-            //     comments.url = '/comments?target_type=new';
-            //     comments.data.type = type;
-            //     comments.data.target_id = reply_id;
-
-            //     var replyDetailPersonView = new Backbone.Marionette.Region({el:"#replyDetailPersonView"});
-            //     var view = new ReplyDetailPersonView({
-            //         model: model
-            //     });
-            //     replyDetailPersonView.show(view); 
-
-            //     var userIfo = new Backbone.Marionette.Region({el:"#userIfo"});
-            //     var view = new ReplyDetailCommentView({
-            //         collection: comments
-            //     });
-            //     userIfo.show(view); 
-
-            //     var count = new Backbone.Marionette.Region({el:"#count"});
-            //     var view = new ReplyDetailCountView({
-            //         model: model
-            //     });
-            //     count.show(view); 
-            //    setTimeout(function(){
-            //         if($(".reply-comment").height() > 550) {
-            //             $(".reply-more").removeClass("blo");
-            //         } else {
-            //             $(".reply-more").addClass("blo");
-            //         }
-            //         $(".reply-detail-ifo").css({
-            //             overflow: "hidden"
-            //         })
-            //     }, 700)
-            // },
             construct: function() {
                 var self = this;
                 this.listenTo(this.model, 'change', this.render);
