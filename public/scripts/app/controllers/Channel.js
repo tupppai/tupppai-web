226d090a6856a1fec31a960f98a4e728ff4baf7d
tupppai <tupppai@tupppaideMac-mini.local>
2015-12-16 11:04:57 +0800

    sty;e

--- public/scripts/app/controllers/Channel.js
+++ public/scripts/app/controllers/Channel.js
@@ -39,12 +39,12 @@ define(['underscore',
             channelWorksPic.show(view);
 
 
-            // channel.data.type = "replies";
-            // var channelWorksFold = new Backbone.Marionette.Region({el:"#channelWorksFold"});
-            // var view = new ChannelFoldView({
-            //     collection: channel
-            // });
-            // channelWorksFold.show(view);
+            channel.data.type = "replies";
+            var channelWorksFold = new Backbone.Marionette.Region({el:"#channelWorksFold"});
+            var view = new ChannelFoldView({
+                collection: channel
+            });
+            channelWorksFold.show(view);
 
         };
     });
