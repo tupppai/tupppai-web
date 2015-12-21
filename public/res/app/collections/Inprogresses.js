54b1f48a425b33467d8f199c31cc534e405402d4
junqiang <whenjonny@gmail.com>
2015-12-21 19:24:43 +0800

    ignore

--- public/res/app/collections/Inprogresses.js
+++
@@ -1,6 +0,0 @@
-define(['app/collections/Base', 'app/models/Inprogress'], function(Collection, Inprogress) {
-    return Collection.extend({
-        model: Inprogress,
-        url: '/inprogresses'
-     });
-}); 
