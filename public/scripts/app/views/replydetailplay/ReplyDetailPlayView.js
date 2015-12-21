0d5cc9a1641c58b015aed6291d723c523d8c8f97
brandwang <402377128@qq.com>
2015-12-22 00:58:03 +0800

    fix bugs

--- public/scripts/app/views/replydetailplay/ReplyDetailPlayView.js
+++ public/scripts/app/views/replydetailplay/ReplyDetailPlayView.js
@@ -121,6 +121,13 @@ define([
                 $(".inp-frame").addClass("blo");
             },
             replyBlo: function(e) {
+                var current_play_icon   = $(e.currentTarget).parents(".reply-ifo").siblings(".inp-frame").find('.play-icon')[0];
+                // 保证唯一id标识
+                var current_play_id     = $(e.currentTarget).parents(".reply-ifo").siblings(".inp-frame").find(".play-inp").attr('name');
+                $(current_play_icon).emojiSelector({
+                    assign: current_play_id,
+                    path: '/scripts/lib/face-selector/face/'
+                });
                 $(e.currentTarget).parents(".reply-ifo").siblings(".inp-frame").removeClass("blo").parents(".user-ifo").siblings(".user-ifo").find(".inp-frame").addClass("blo");
             },
             picScroll: function(e) {
