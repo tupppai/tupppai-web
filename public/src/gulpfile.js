98146689acfba7ea9247b12af12e91a10051467e
junqiang <whenjonny@gmail.com>
2015-12-21 16:56:02 +0800

    rename

---
+++ public/src/gulpfile.js
@@ -0,0 +1,54 @@
+var gulp    = require('gulp'),
+	shell   = require('gulp-shell'),
+    jshint  = require('gulp-jshint'),
+    uglify  = require('gulp-uglify'),
+    clean   = require('gulp-clean'),
+    rename  = require('gulp-rename'),
+    rev     = require('gulp-rev'),
+    revCollector = require('gulp-rev-collector'),
+	less    = require("gulp-less");
+
+gulp.task('clean', function() {
+    return gulp.src('../res', {read: false}).pip(clean());
+});
+
+gulp.task('css', function() {
+    return gulp.src(['less/*.less'])
+        .pipe(less()).pipe(rename(function(path) { 
+            path.basename += '.min';
+            path.extname   = '.css';
+        }))
+        .pipe(gulp.dest('../css'))
+        .pipe(rev())
+        .pipe(rev.manifest())
+        .pipe(gulp.dest('../css/rev'));
+});
+
+gulp.task('app', function() {
+    return gulp.src(['./**'])
+        .pipe(gulp.dest('../build'));//todo: scripts
+});
+
+gulp.task('less', function() {
+    return gulp.src(['less/*.less'])
+        .pipe(less())
+        .pipe(gulp.dest('../css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./**', ['css', 'app']); 
+});
+
+gulp.task('rev', function() {
+    return gulp.src(['../css/rev/**/*.json', '../*.html']);
+});
+
+gulp.task('rjs', shell.task([
+	'node r.js -o build.js'
+]));
+
+gulp.task('release', ['rjs']);
+
+gulp.task('default', function() {
+	//TODO
+});
