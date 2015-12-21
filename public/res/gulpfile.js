8d32d9a396b8ea87ac56e3d25e6e30026ff7dc4c
tupppai <tupppai@tupppaideMac-mini.local>
2015-12-21 17:47:00 +0800

    css

--- public/res/gulpfile.js
+++ public/res/gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('clean', function() {
 gulp.task('css', function() {
     return gulp.src(['less/*.less'])
         .pipe(less()).pipe(rename(function(path) { 
-            path.basename += '.min';
+            //path.basename += '.min';
             path.extname   = '.css';
         }))
         .pipe(gulp.dest('../css'))
@@ -40,7 +40,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('rev', function() {
-    return gulp.src(['../css/rev/**/*.json', '../*.html']);
+    return gulp.src(['../css/rev/**/*.json', '../index.html']);
 });
 
 gulp.task('rjs', shell.task([
