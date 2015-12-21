88a5d1cddc25369ef5ee66af3a4848b10ac5b877
junqiang <whenjonny@gmail.com>
2015-12-21 19:24:43 +0800

    ignore

--- public/res/app/controllers/Login.js
+++
@@ -1,9 +0,0 @@
-define(['app/views/LoginView'],
-    function (LoginView) {
-        "use strict";
-
-        return function() {
-            var view = new LoginView();
-            window.app.modal.show(view);
-        };
-    });
