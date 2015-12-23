30fda501a45c1de6a54c39fcd0c86bfef3ede2f7
Valentino <valentino@163.com>
2015-12-23 22:26:08 +0800

    css

--- public/src/app/views/upload/UploadingReplyView.js
+++ public/src/app/views/upload/UploadingReplyView.js
@@ -41,7 +41,6 @@ define([ 'common', 'uploadify','app/views/Base'],
                         desc: desc,
                     }, function(data) {
                         $.fancybox.close();
-                        hisgtory.go(-1);
                         location.load();    
                             $('.title-bar').removeClass("hide");
                             $('.header-back').removeClass("height-reduce");
