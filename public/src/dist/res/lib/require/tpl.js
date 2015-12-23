fe59b2650590008a5657f5589d61f8a433bc0641
nunqiang <whenjonny@gmail.com>
2015-12-23 10:51:23 +0800

    deploy dist

--- public/src/dist/res/lib/require/tpl.js
+++ public/src/dist/res/lib/require/tpl.js
@@ -15,6 +15,7 @@
  java: false */
 
 (function () {
+//>>excludeStart('excludeTpl', pragmas.excludeTpl)
     var progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'],
 
         xmlRegExp = /^\s*<\?xml(\s)+version=[\'\"](\d)*.(\d)*[\'\"](\s)*\?>/im,
@@ -57,8 +58,10 @@
              return data ? func(data) : func;
              /**/
         };
+//>>excludeEnd('excludeTpl')
 
     define(function () {
+//>>excludeStart('excludeTpl', pragmas.excludeTpl)
         var tpl;
 
         var get, fs;
@@ -195,6 +198,7 @@
                 }
             }
         };
+//>>excludeEnd('excludeTpl')
         return function () {
         };
     });
