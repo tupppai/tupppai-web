63f27021fceb44f1b3b98a4c4076265ac21ee8fc
nunqiang <whenjonny@gmail.com>
2015-12-25 11:46:44 +0800

    deploy dist

---
+++ public/src/dist/res/app/controllers/Trend.js
@@ -0,0 +1,19 @@
+ define(['underscore','app/views/trend/TrendView','app/collections/Replies'],
+    function (_, TrendView, Replies) {
+        "use strict";
+
+        return function() {
+
+            setTimeout(function(){
+                $("title").html("图派-动态页面");
+                $('.header-back').removeClass("height-reduce");
+            },100);
+
+        	var replies = new Replies;
+        	replies.url = 'timeline';
+            var view = new TrendView({collection: replies});
+            
+            window.app.content.show(view);
+        
+        };
+    });
---
+++ public/src/dist/res/app/views/channel/ChannelNavView.js
@@ -0,0 +1,18 @@
+ define([ 
+        'app/views/Base',
+        'tpl!app/templates/channel/ChannelNavView.html'
+       ],
+    function (View, template) {
+
+        "use strict";
+        return View.extend({
+            tagName: 'div',
+            className: '',
+            template: template,
+            construct: function () {
+                this.listenTo(this.collection, 'change', this.render);
+                this.collection.loading();
+            }
+           
+        });
+    });
