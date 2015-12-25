ef7601b188a22381d9b5b1954197dd89c7b3ec17
Valentino <valentino@163.com>
2015-12-25 11:51:49 +0800

    css

--- public/src/app/views/index/IndexView.js
+++ public/src/app/views/index/IndexView.js
@@ -57,16 +57,7 @@ define(['app/views/Base', 'tpl!app/templates/index/IndexView.html'],
             onRender: function() {
                 
             	$(".tupai-index").addClass("active").siblings().removeClass("active");
-                // setTimeout(function(){
-                //     var id = $("body").attr("data-uid");
-                //     if( id ) {
-                //         $(".login-popup").addClass("hide");
-                //         $(".ask-uploading-popup-hide").removeClass('hide');
-                //     } else {
-                //         $(".ask-uploading-popup-hide").addClass('hide');
-                //         $(".login-popup").removeClass("hide");
-                //     }
-                // },500);
+   
             },
             indexFadeIn: function(e) {
                 $(e.currentTarget).find(".index-artwork").stop(true, true).fadeIn(1500);
