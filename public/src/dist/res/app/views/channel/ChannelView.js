b702da6f07f49080606a400538c274c3cc78f7c3
skys215 <skys215@gmail.com>
2015-12-28 17:59:44 +0800

    deploy dist

--- public/src/dist/res/app/views/channel/ChannelView.js
+++ public/src/dist/res/app/views/channel/ChannelView.js
@@ -34,6 +34,7 @@
                 "click #check_more" : "checkMore"
             },
             onRender: function() {
+            
                 $(window).resize(function(){
                     var width = ($(window).width());
                     if(width > 1351) {
@@ -45,8 +46,10 @@
             },
             checkMore:function() {
                 var category_id = $(".bgc-change").attr("data-id");
+                $(".channel-all-ask").removeClass('hide');
                 $(".demand-p").addClass('hide');
-                $(".channel-works").addClass('hide');
+                $("#channelWorksPic").empty();
+                $(".channel-works-header").addClass('hide');
 
                 var ask = new Asks;
                 ask.data.width = 300;
@@ -101,12 +104,12 @@
             },
             channelWidth: function(e) {
                 if(e.type == "mouseover") {
-                    $(e.currentTarget).siblings(".view-details").animate({
+                    $(e.currentTarget).find(".view-details").animate({
                         width: "20px"
                     }, 500);
                 }
                 if(e.type == "mouseleave") {
-                    $(e.currentTarget).siblings(".view-details").stop(true, true).animate({
+                    $(e.currentTarget).find(".view-details").stop(true, true).animate({
                         width: "0px"
                     }, 500);
                 }
@@ -223,6 +226,7 @@
                 $("#askflowsShow").empty();
                 $(".demand-p").removeClass('hide');
                 $(".channel-works").removeClass('hide');
+                $(".channel-all-ask").addClass('hide');
                 $('.header-back').addClass("height-reduce");
                 $(".channel-header").find(".header-nav").removeClass('bgc-change');
                 $(e.currentTarget).addClass("bgc-change");
@@ -280,6 +284,7 @@
                     $(".channel-ask").addClass("hide");
                     $(".channel-reply").removeClass("hide");
                 }
+
 
                 $(".pic-icon").css({
                     backgroundPosition: "-128px -501px"
