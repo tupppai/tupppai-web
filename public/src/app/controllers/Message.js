1a83c7561eb45400aac44d805d23676300c494a7
tupppai <tupppai@tupppaideMac-mini.local>
2015-12-29 15:51:44 +0800

    message

--- public/src/app/controllers/Message.js
+++ public/src/app/controllers/Message.js
@@ -3,9 +3,10 @@ define([
         'app/collections/Messages',
         'app/views/message/MessageView', 
         'app/views/message/MessageItemView',
+        'app/views/message/MessagePraiseView',
         'app/views/message/CommentItemView'
 	   ],
-    function ( Message, Messages, MessageView, messageItemView, CommentItemView) {
+    function ( Message, Messages, MessageView, messageItemView, MessagePraiseView, CommentItemView) {
         "use strict";
 
         return function(type, uid) {
@@ -19,22 +20,28 @@ define([
             window.app.content.show(view);
 
 
-            if( type != 'comment') {
+            if( type == 'comment') {
+
+                var commentListRegion = new Backbone.Marionette.Region({el:"#message-item-list"});
+                var view = new CommentItemView({
+                    collection: messages 
+                });
+                commentListRegion.show(view);
+
+            }  else if (type == "praise") {
+                var commentListRegion = new Backbone.Marionette.Region({el:"#message-item-list"});
+                var view = new MessagePraiseView({
+                    // collection: messages 
+                });
+                commentListRegion.show(view);
+            } else {
                 var messageListRegion = new Backbone.Marionette.Region({el:"#message-item-list"});
                 var view = new messageItemView({
                     collection: messages 
             });
                 messageListRegion.show(view);
                 
-            } else {
-
-            var commentListRegion = new Backbone.Marionette.Region({el:"#message-item-list"});
-            var view = new CommentItemView({
-                collection: messages 
-            });
-                commentListRegion.show(view);
-
-            }
+            } 
 
             $("title").html("图派-消息");
             
@@ -45,10 +52,12 @@ define([
             } else if( type == "reply" ) {
                 $(".nav-title").html("帖子回复");
             } else if( type == "comment") {
-                $(".nav-title").html("发出的评论");
+                $(".nav-title").html("<span class='message-issue nav-change'>发出的评论</span>|<span class='message-receive'>收到的评论</span>");
             } else if( type === "system") {
                 $(".nav-title").html("系统通知");
-            }
+            } else if( type == "praise") {
+                $(".nav-title").html("<span class='message-issue nav-change'>发出的赞</span>|<span class='message-receive'>收到的赞</span>");
+            };
 
             
         };
