7c2be63f5e1c61b514702c90cbaba4bb31551e86
junqiang <whenjonny@gmail.com>
2016-01-06 10:55:52 +0800

    library alidayu sms

--- library/topsdk/Alidayu.php
+++ library/topsdk/Alidayu.php
@@ -23,8 +23,9 @@ class Alidayu {
         //公共回传参数，在“消息返回”中会透传回该参数；举例：用户可以传入自己下级的会员ID
         //，在消息返回时，该会员ID会包含在内，用户可以根据该会员ID识别是哪位会员使用了你的应用
         $req->setSmsType("normal");   //短信类型，传入值请填写normal
-        $req->setSmsFreeSignName("注册验证");//短信签名,等图派审核通过替换成图派
-        $req->setSmsParam("{\"code\":\"$code\",\"product\":\"图派\"}");  
+        $req->setSmsFreeSignName("图派");//短信签名,等图派审核通过替换成图派
+        //$req->setSmsParam("{\"name\":\"$code\",\"code\":\"$code\",\"product\":\"图派\"}");  
+        $req->setSmsParam("{\"name\":\"$code\"}");  
         //短信模板变量，传参规则{"key":"value"}，
         //key的名字须和申请模板中的变量名一致
         //，多个变量之间以逗号隔开。示例：针对模板“验证码${code}，
