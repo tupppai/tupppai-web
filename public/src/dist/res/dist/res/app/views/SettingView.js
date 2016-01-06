23bce2b3df917e1de72b666eeef2dfb46db5e404
nunqiang <whenjonny@gmail.com>
2016-01-06 11:17:33 +0800

    merge from backend

--- public/src/dist/res/dist/res/app/views/SettingView.js
+++
@@ -1,1 +0,0 @@
-define(["common","app/views/Base","tpl!app/templates/SettingView.html"],function(e,t,n){"use strict";return t.extend({tagName:"div",className:"",template:n,events:{"click .base-nav":"navBar"},navBar:function(e){var t=$(e.currentTarget).attr("data-type");location.href="#setting/"+t,setTimeout(function(){location.reload()},100)}})});
