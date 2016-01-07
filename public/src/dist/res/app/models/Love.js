9ba3a236a4452c5baa6b4e5db0da8dc8779becab
nunqiang <whenjonny@gmail.com>
2016-01-07 15:13:36 +0800

    deploy dist

--- public/src/dist/res/app/models/Love.js
+++ public/src/dist/res/app/models/Love.js
@@ -1,1 +1,22 @@
-define(["app/models/Base"],function(e){return e.extend({url:"/like/",defaults:{id:0,num:0,is_star:""},construct:function(){},save:function(e){this.fetch({data:this.toJSON(),success:function(t){e&&e(t)}})}})});
+define(['app/models/Base'], function(Model) {
+    return Model.extend({
+        url: '/like/',
+        defaults: {
+            id: 0,
+            num: 0,
+            is_star: ''
+        },
+        construct: function() {
+
+        },
+        save: function(callback){ 
+            this.fetch({
+                data: this.toJSON(),
+                success: function(data) {
+                    callback && callback(data);
+                }
+            });
+        }
+    });
+
+}); 
