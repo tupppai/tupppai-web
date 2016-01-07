f9779d61a5a5cad602dc5e6b2aabd823f375da09
junqiang <whenjonny@gmail.com>
2016-01-07 11:26:45 +0800

    change some base settings

---
+++ bootstrap/constant.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 友盟相关 **/
+//友盟APPKEY  MASTER SECRET
+define('UMENG_IOS_APPKEY', env('UMENG_IOS_APPKEY'));
+define('UMENG_IOS_MASTER_SECRET',env('UMENG_IOS_MASTER_SECRET'));
+
+define('UMENG_ANDROID_APPKEY', env('UMENG_ANDROID_APPKEY'));
+define('UMENG_ANDROID_MASTER_SECRET',env('UMENG_ANDROID_MASTER_SECRET'));
+
+//友盟SECRET
+define('UMENG_SECRET',env('UMENG_SECRET'));
+
+//玄武短信发送平台 用户名和密码
+define('XW_USERNAME', env('XW_USERNAME'));
+define('XW_PASSWORD', env('XW_PASSWORD'));
+
+//微信AppKEY
+define('WX_APPID', env('WX_APPID'));
+define('WX_APPSECRET', env('WX_APPSECRET'));
+
+define('APP_NAME', '图派');
