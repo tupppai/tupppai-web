644a7e76a66d088a3fa1fa37a2f8413ecc74865e
nunqiang <whenjonny@gmail.com>
2016-01-08 18:01:10 +0800

    deploy dist

--- public/src/dist/res/app/views/register/LoginView.js
+++ public/src/dist/res/app/views/register/LoginView.js
@@ -1,1 +1,22 @@
-define(["app/views/Base","app/models/User","tpl!app/templates/register/LoginView.html"],function(e,t,n){"use strict";return e.extend({tagName:"div",className:"",template:n,construct:function(){var e=this;$(".login-popup").fancybox({afterShow:function(){$("#login_btn").unbind().bind("click",account.login),$(".login-panel input").keyup(account.login_keyup),$("#login_password").keypress(account.keypress)}})}})});
+define(['app/views/Base', 'app/models/User', 'tpl!app/templates/register/LoginView.html'],
+    function (View, User, template) {
+        "use strict";
+        
+        return View.extend({
+            tagName: 'div',
+            className: '',
+            template: template,
+            construct: function () {
+                var self = this;
+                
+                $(".login-popup").fancybox({
+                    afterShow: function(){
+                        $("#login_btn").unbind().bind("click",account.login);
+                        $('.login-panel input').keyup(account.login_keyup);
+                        $('#login_password').keypress(account.keypress);
+                    }
+                });
+            },
+        
+        });
+    });
