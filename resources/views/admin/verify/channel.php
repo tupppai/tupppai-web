d8ee974024d0cc4670050e2a9fc4f39f0322e731
ff笔尖 <104260033@qq.com>
2016-01-12 16:22:12 +0800

    另使用 TempPaginate

--- resources/views/admin/verify/channel.php
+++ resources/views/admin/verify/channel.php
@@ -120,7 +120,7 @@ jQuery(document).ready(function() {
         $('li.all').addClass('active');
     }
 
-    table = new Paginate();
+    table = new TempPaginate();
     table.init({
         src: $('#thread-data'),
         url: '/verify/list_category_threads?category_id='+category_id+'&category_type=channels&status='+status,
@@ -191,4 +191,79 @@ jQuery(document).ready(function() {
 
 
 });
+
+/***
+ Wrapper/Helper Class for datagrid based on jQuery Datatable Plugin
+ ***/
+var TempPaginate = function() {
+    var options = {
+        src: null,
+        url: null,
+        template: null,
+
+        count       : 50,
+        start       : 1,
+        display     : 10,
+        border                  : false,
+        text_color              : '#79B5E3',
+        background_color        : 'none',
+        text_hover_color        : '#2573AF',
+        background_hover_color  : 'none',
+        images      : false,
+        mouse       : 'press'
+    };
+
+    return {
+        init: function(opts) {
+            for(var i in options) {
+                if(opts[i]) options[i] = opts[i];
+            }
+            options.onChange = this.submitFilter;
+            options.success  = opts.success;
+            var Paginate = this;
+            options.src.append('<div id="paginate-content"></div>');
+            options.src.append('<div id="paginate-pagebar"></div>');
+            $('button.form-filter[type="submit"]').click(function() {
+                Paginate.submitFilter();
+            });
+            this.submitFilter(1);
+        },
+        submitFilter: function(index){
+            if(!index) index = options.start;
+            var params= {};
+            var forms = $(".form-filter");
+            _.each(forms, function(row){
+                if(row.name && row.value)
+                    params[row.name] = row.value;
+            });
+            params['page'] = index;
+
+            $.get(options.url, params, function(data){
+                $("#paginate-content").empty();
+                data = JSON.parse(data);
+
+                options.count = data.recordsTotal/options.display;
+                if(options.count > parseInt(options.count)) {
+                    options.count = parseInt(options.count) + 1;
+                }
+                //todo: error reporting
+                results = data.data;
+                for(var i in results){
+                    $("#paginate-content").append(options.template(results[i]));
+                }
+                if(results.length == 0){
+                    $("#paginate-content").append('<div style="margin-top: 20px; text-align:center">空记录</div>');
+                }
+                options.success && options.success(results);
+
+                options.start = index;
+                if(options.count > 1) {
+                    $("#paginate-pagebar").paginate(options);
+                }
+            });
+        }
+    };
+};
+
+
 </script>
