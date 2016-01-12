b3a944ca2250c288f2c415d27032f744545f32cb
ff笔尖 <104260033@qq.com>
2016-01-12 16:19:52 +0800

    后台 － 小秘书 －推荐banner － 添加banner

--- config/code.php
+++ config/code.php
@@ -6,5 +6,6 @@ return [
     'BACKEND_HANDLE_LOVE' => 'LoveHandle',
     'BACKEND_HANDLE_ROLE_SETROLE' => 'RoleSetRoleHandle',
     'BACKEND_HANDLE_ROLE_PERMISSION' => 'RoleSavePermissionHandle',
+    'BACKEND_HANDLE_BANNER_SAVE' => 'BannerSaveHandle',
     'BACKEND_HANDLE_RECOMMENDATION_CHG_STAT' => 'RecommendationChgStatHandle',
 ];
