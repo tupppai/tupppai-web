488ebb57143cb8ec7e4902ffe661951b6f9a7050
skys215 <skys215@gmail.com>
2016-01-13 16:34:08 +0800

    Added status and create by, update by for products.

--- database/migrations/2016_01_13_093029_create_table_products.php
+++ database/migrations/2016_01_13_093029_create_table_products.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TradeAddTableProducts extends Migration
+class CreateTableProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -18,6 +18,11 @@ class TradeAddTableProducts extends Migration
             $table->string('desc')->nullable();  //商品描述
             $table->string('remark')->nullable();//备注（后台人员用，不公开）
             $table->double('price');//商品单价
+            $table->integer('status'); //商品状态
+            $table->integer('create_by');
+            $table->integer('create_time');
+            $table->integer('update_time');
+            $table->integer('update_by');
             //暂不添加  商品单位，折扣等信息
         });
     }
