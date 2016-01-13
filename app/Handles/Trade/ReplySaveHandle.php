9ea48bab39161c08ee2d43139e7cc7ee2bc3f17c
ff笔尖 <104260033@qq.com>
2016-01-13 13:18:29 +0800

    求P交易

--- app/Handles/Trade/ReplySaveHandle.php
+++ app/Handles/Trade/ReplySaveHandle.php
@@ -6,8 +6,60 @@ namespace App\Handles\Trade;
 
 class ReplySaveHandle
 {
-    public function handle()
+    public function handle(Event $event)
     {
-        
+        $asks = $event->arguments['ask'];
+        //1,创建订单
+        $orderId = $this->createOrderId($asks->id);
+        //2,获取订单
+        $orderId = $this->getOrderId($asks->id);
+        //3,提交支付
+        $this->pay($orderId);
+        //4,写流水
+        $this->transaction($order);
+
+
+
+    }
+    /*
+     * 判断是否在三天以内
+     * 三天以内由发起求P用户支付
+     * 超过三天由默认uid支付
+     * */
+    public function checkPayUser($ask)
+    {
+
+    }
+    /*
+ * 生成订单
+ * */
+    public function createOrder($asks)
+    {
+        return true;
+    }
+    /*
+     * 获得订单
+     * return orderId
+     * */
+    public function getOrderId($asksId)
+    {
+        $orderId = 0;
+        return $orderId;
+    }
+    /*
+     * 支付订单
+     * */
+    public function pay($orderId)
+    {
+        //支付状态
+        $status = 0;
+        return $status;
+    }
+    /*
+     * 写流水
+     * */
+    public function transaction($order)
+    {
+        return true;
     }
 }
