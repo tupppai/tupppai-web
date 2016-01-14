5b3f3bd673df4a8149177c3188bcebb3523a66c8
ff笔尖 <104260033@qq.com>
2016-01-14 15:47:54 +0800

    格式化代码

--- app/Handles/Trade/AsksSaveHandle.php
+++ app/Handles/Trade/AsksSaveHandle.php
@@ -19,20 +19,20 @@ class AsksSaveHandle extends Trade
         $amount = $this->getGoodsAmount(1);
 
         //检查扣除商品费用后,用户余额是否充足
-        $checkUserBalance = $this->checkUserBalance($ask->uid,$amount);
-        if(!$checkUserBalance) {
+        $checkUserBalance = $this->checkUserBalance($ask->uid, $amount);
+        if (!$checkUserBalance) {
             //写流水交易失败,余额不足
-            $this->freezeAccount($ask->uid, $amount, tUser::getBalance($ask->uid),tAccount::ACCOUNT_FAIL_STATUS, '余额不足');
+            $this->freezeAccount($ask->uid, $amount, tUser::getBalance($ask->uid), tAccount::ACCOUNT_FAIL_STATUS, '余额不足');
             return error('TRADE_USER_BALANCE_ERROR');
         }
 
         //操作psgod_trade库
-        DB::connection('db_trade')->transaction(function() use($ask,$amount){
+        DB::connection('db_trade')->transaction(function () use ($ask, $amount) {
             //冻结(求P用户)金额
-            $this->freeze($ask->uid,$amount);
+            $this->freeze($ask->uid, $amount);
             //写冻结流水
             $userGoodsBalance = tUser::getBalance($ask->uid);
-            $this->freezeAccount($ask->uid, $amount, $userGoodsBalance,tAccount::ACCOUNT_SUCCEED_STATUS);
+            $this->freezeAccount($ask->uid, $amount, $userGoodsBalance, tAccount::ACCOUNT_SUCCEED_STATUS);
             //恢复求P状态为常态
             $this->setAskStatus($ask);
         });
