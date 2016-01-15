3869f32610161f7d03875836beff38780437b5fd
ff笔尖 <104260033@qq.com>
2016-01-15 17:36:33 +0800

    增加付款事务

--- app/Jobs/CheckUserPayReply.php
+++ app/Jobs/CheckUserPayReply.php
@@ -6,6 +6,7 @@ use App\Services\Reply as sReply;
 use App\Trades\Order as tOrder;
 use App\Trades\User as tUser;
 use Illuminate\Contracts\Bus\SelfHandling;
+use Illuminate\Support\Facades\DB;
 use Log;
 
 class CheckUserPayReply extends Job
@@ -36,7 +37,7 @@ class CheckUserPayReply extends Job
     {
         //获取7天内作品点赞数最高的用户
         $sellerUid = sReply::getAskForReplyMaxLike($this->askId);
-        //获取ask保存amount金额
+        //获取ask保存的amount金额
         $ask = sAsk::getAskById($this->askId);
         $amount = $ask->amount;
 
@@ -44,20 +45,26 @@ class CheckUserPayReply extends Job
         $orderInfo = sProduct::getProductById(1);
         $orderInfo['price'] = $amount;
 
-        //是否是用户支付
-        if (sAsk::isAskHasFirstReplyXDay($this->askId, 3)) {
-            $uid = $this->uid;
-            //解除冻结
-            tUser::unFreezeBalance($uid, $amount);
-        } else {
-            $uid = tUser::SYSTEM_USER_ID;
-        }
-        //用户支付 传入购买用户ID
-        $order = new tOrder($uid);
-        //生成订单 传入卖家ID
-        $order->createOrder($sellerUid, $amount, $orderInfo);
-        //支付订单
-        tUser::pay($uid, $sellerUid, $amount);
+        //检查Ask第一个作品是否是3天以内发送
+        $isAskHasFirstReplyXDay = sAsk::isAskHasFirstReplyXDay($this->askId, 3);
+
+        DB::connection('db_trade')->transaction(function () use ($sellerUid, $orderInfo, $isAskHasFirstReplyXDay, $amount) {
+
+            //是否是用户支付
+            if ($isAskHasFirstReplyXDay) {
+                $uid = $this->uid;
+                //解除冻结
+                tUser::unFreezeBalance($uid, $amount);
+            } else {
+                $uid = tUser::SYSTEM_USER_ID;
+            }
+            //用户支付 传入购买用户ID
+            $order = new tOrder($uid);
+            //生成订单 传入卖家ID
+            $order->createOrder($sellerUid, $amount, $orderInfo);
+            //支付订单
+            tUser::pay($uid, $sellerUid, $amount);
+        });
     }
 
 }
