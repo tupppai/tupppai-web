428a3248f9aa47ce404f7fd8c34d073b9d0882dd
ff笔尖 <104260033@qq.com>
2016-01-15 14:04:33 +0800

    Product ＊1000

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,6 +3,17 @@
 class Product extends ModelBase{
 	public $table = 'products';
 
+	public function setPriceAttribute($value)
+	{
+		$this->attributes['price'] = $value * 1000;
+	}
+
+	public function getPriceAttribute($value)
+	{
+		return $value / 1000;
+	}
+
+
 	public function get_product_by_id( $id ){
 		return $this->find( $id );
 	}
