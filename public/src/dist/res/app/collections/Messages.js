71b78965ffacb541598811cd194a747ac04e1732
billqiang <whenjonny@gmail.com>
2016-01-18 15:44:12 +0800

    publish dist

--- public/src/dist/res/app/collections/Messages.js
+++ public/src/dist/res/app/collections/Messages.js
@@ -1,13 +1,1 @@
-define(['app/collections/Base', 'app/models/Message'], function(Collection, Message) {
-    return Collection.extend({
-        model: Message,
-        url: '/messages',
-        initialize: function() {
-            this.data = {
-                type: 'normal',
-                page: 0,
-                size: 10
-            }
-        }
-     });
-}); 
+define(["app/collections/Base","app/models/Message"],function(e,t){return e.extend({model:t,url:"/messages",initialize:function(){this.data={type:"normal",page:0,size:10}}})});
--- public/src/dist/res/app/controllers/HotFlows.js
+++ public/src/dist/res/app/controllers/HotFlows.js
@@ -1,15 +1,1 @@
-define(['underscore', 'app/collections/Replies', 'app/views/hot/HotFlowsView'],
-    function (_, Replies, HotFlowsView) {
-        "use strict";
-
-        return function() {
-            var replies = new Replies;
-            replies.data.width = 300;
-
-            var view = new HotFlowsView({
-                collection: replies
-            });
-
-            window.app.content.show(view);
-        };
-    });
+define(["underscore","app/collections/Replies","app/views/hot/HotFlowsView"],function(e,t,n){"use strict";return function(){var e=new t;e.data.width=300;var r=new n({collection:e});window.app.content.show(r)}});
