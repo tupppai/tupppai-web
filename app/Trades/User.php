b822c957ac3dc91bd5c07aef81107d964bca31a4
ff笔尖 <104260033@qq.com>
2016-01-18 10:56:32 +0800

    作品

--- app/Trades/User.php
+++ app/Trades/User.php
@@ -58,6 +58,7 @@ class User extends TradeBase
         $balance = self::getBalance($uid);
         $balance = ($balance + $amount);
         self::setBalance($uid, $balance);
+        tAccount::writeAccount($uid, $amount, self::getBalance($uid), tAccount::STATUS_ACCOUNT_SUCCEED, tAccount::TYPE_ACCOUNT_INCOME, '解冻回退金额');
         return $balance;
     }
 
@@ -127,6 +128,7 @@ class User extends TradeBase
         $freezing = self::getFreezing($uid);
         $freezing = ($freezing - $amount);
         self::setFreezing($uid, $freezing);
+        tAccount::writeAccount($uid, $amount, self::getBalance($uid), tAccount::STATUS_ACCOUNT_SUCCEED, tAccount::TYPE_ACCOUNT_UNFREEZE, '解除冻结金额');
         return $freezing;
     }
 
