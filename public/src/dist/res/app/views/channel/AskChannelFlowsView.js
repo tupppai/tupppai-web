e0b47092371de444df545c826ab90dd7a8565677
skys215 <skys215@gmail.com>
2016-01-19 17:02:53 +0800

    deploy dist

--- public/src/dist/res/app/views/channel/AskChannelFlowsView.js
+++ public/src/dist/res/app/views/channel/AskChannelFlowsView.js
@@ -1,1 +1,56 @@
-define(["imagesLoaded","app/views/Base","app/collections/Asks","tpl!app/templates/channel/AskFlowsView.html"],function(e,t,n,r){"use strict";return t.extend({collection:n,tagName:"div",className:"ask-container grid",template:r,events:{"click .download":"download"},construct:function(){this.listenTo(this.collection,"change",this.render),this.scroll(),this.collection.loading(),$("title").html("图派-原图"),$(".header-back").addClass("height-reduce"),$(document).on("mouseenter",".person-message-1",function(){$(this).animate({opacity:1},11),$(this).parents(".grid-item").find(".person-message").animate({opacity:0},100)}),$(document).on("mouseleave",".person-message-1",function(){$(this).animate({opacity:0},200),$(this).parents(".grid-item").find(".person-message").animate({opacity:1},500)}),$(document).on("mouseenter",".ask-main",function(){$(this).addClass("hover"),$(this).parents(".grid-item").find(".person-message").addClass("hide")}),$(document).on("mouseenter",".ask-desc",function(){$(this).parents(".grid-item").find(".ask-main").addClass("hover"),$(this).parents(".grid-item").find(".person-message").addClass("hide")}),$(document).on("mouseleave",".ask-mouseenter",function(){$(this).find(".person-message").removeClass("hide")})}})});
+    define(['imagesLoaded',
+    		'app/views/Base',
+    		'app/collections/Asks', 
+    		'tpl!app/templates/channel/AskFlowsView.html'
+	       ], function (imagesLoaded, View, Asks, template) {
+
+        "use strict";
+        
+        return View.extend({
+            collection: Asks,
+            tagName: 'div',
+            className: 'ask-container grid',
+            template: template,
+            events: {
+                "click .download" : "download",
+            },
+         
+            construct: function () {
+                this.listenTo(this.collection, 'change', this.render);
+                this.scroll();
+                this.collection.loading();
+
+                $("title").html("图派-原图");
+                $('.header-back').addClass("height-reduce");
+                
+                $(document).on('mouseenter', '.person-message-1', function() {
+                    $(this).animate({
+                        'opacity' : 1
+                    },11);
+                    $(this).parents('.grid-item').find('.person-message').animate({
+                        'opacity' : 0
+                    },100);
+                });
+                $(document).on('mouseleave', '.person-message-1', function() {
+                    $(this).animate({
+                        'opacity' : 0
+                    },200);
+                    $(this).parents('.grid-item').find('.person-message').animate({
+                        'opacity' : 1
+                    },500);
+                });
+                $(document).on('mouseenter', '.ask-main', function() {
+                    $(this).addClass('hover');
+                    $(this).parents('.grid-item').find('.person-message').addClass('hide');
+                });
+                $(document).on('mouseenter', '.ask-desc', function() {
+                    $(this).parents('.grid-item').find('.ask-main').addClass('hover');
+                    $(this).parents('.grid-item').find('.person-message').addClass('hide');
+                });
+                $(document).on('mouseleave', '.ask-mouseenter', function() {
+                    $(this).find('.person-message').removeClass('hide');
+                    
+                });
+            },
+        });
+    });
