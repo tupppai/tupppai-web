e3b8cbc8791f835ba8f4ac3844cafb4df277947e
Valentino <valentino@163.com>
2016-01-20 18:47:59 +0800

    change:meney pay function

--- public/src/app/views/money/MoneyView.js
+++ public/src/app/views/money/MoneyView.js
@@ -13,7 +13,7 @@
                 "click .recharge": "paymentMethod",
                 "click .ali, .weix": "recharge",
                 "click .money-cancel": "moneyCancel",
-                "click .weixinPay": "weixinPay" 
+                "click #confirm": "submitPay" 
             },
             paymentMethod: function() {
                 $(".fonbox").removeClass("blo");
@@ -21,7 +21,7 @@
             moneyCancel: function() {
                 $(".fonbox").addClass("blo");
             },
-            weixinPay:function(e) {
+            submitPay:function(e) {
                 var uid = $(".user-message").attr("data-uid");
                 var amount = document.getElementById('amount').value * 1000;
                 var channel = $(e.currentTarget).attr("data-pay");
@@ -31,37 +31,11 @@
                     channel: channel,
                     amount: amount
                 },function(data){
-                    /*
-                    if (xhr.readyState == 4 && xhr.status == 200) {
-                        console.log(xhr.responseText);
-                        pingpp.createPayment(xhr.responseText, function(result, err) {
-                            console.log(result);
-                            console.log(err);
-                        });
-                    }
-                    */
+         
                 });
                 
-                // var xhr = new XMLHttpRequest();
-                // xhr.open("POST", pay_url, true);
-                // xhr.setRequestHeader("Content-type", "application/json");
-                // xhr.send(JSON.stringify({
-                //     channel: channel,
-                //     amount: amount
-                // }));
-                
-                // xhr.onreadystatechange = function () {
-                //     if (xhr.readyState == 4 && xhr.status == 200) {
-                //         console.log(xhr.responseText);
-                //         pingpp.createPayment(xhr.responseText, function(result, err) {
-                //             console.log(result);
-                //             console.log(err);
-                //         });
-                //     }
-                // }
             },
             recharge: function(e) {
-                debugger;
                 var data_pay = $(e.currentTarget).attr("data-pay");
                 $("#confirm").attr("data-pay", data_pay);
                 $(".payment-method").addClass("blo");
