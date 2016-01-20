0b8ffe6d21ab657bca6ddeb82e0e9230d6ccfd43
tupppai <tupppai@tupppaideMac-mini.local>
2016-01-20 18:05:25 +0800

    moneyview.js

--- public/src/app/views/money/MoneyView.js
+++ public/src/app/views/money/MoneyView.js
@@ -10,23 +10,22 @@
             className: '',
             template: template,
             events: {
-<<<<<<< HEAD
                 "click .recharge": "paymentMethod",
                 "click .ali, .weix": "recharge",
-                "click .ali, .weix": "recharge",
                 "click .money-cancel": "moneyCancel",
+                "click .weixinPay": "weixinPay" 
             },
             paymentMethod: function() {
-=======
-                "click .weixinPay" : "weixinPay",
-                "click .recharge": "recharge"
+                $(".fonbox").removeClass("blo");
+            },
+            moneyCancel: function() {
+                $(".fonbox").addClass("blo");
             },
             weixinPay:function() {
                 var uid = $(".user-message").attr("data-uid");
                 var amount = document.getElementById('amount').value * 1000;
                 var channel = 'alipay_wap';
                 var pay_url = "ping/pay";
-debugger;
                 $.post('pay_url',{
                     uid: uid,
                     channel: channel,
@@ -59,10 +58,6 @@ debugger;
                 //         });
                 //     }
                 // }
-            },
-            recharge: function() {
->>>>>>> 96be72440ae62079b4c140099bd22c8b99561cb3
-                $(".fonbox").removeClass("blo");
             },
             recharge: function() {
                 $(".payment-method").addClass("blo");
