4a3e9ee0d3717d8797340a5ca21a286a357d7977
tupppai <tupppai@tupppaideMac-mini.local>
2016-01-26 18:31:12 +0800

    view

--- public/src/app/views/channel/ChannelNavView.js
+++ public/src/app/views/channel/ChannelNavView.js
@@ -47,6 +47,14 @@
                         });
                     };
                 }, 1500);
+                if($(window).width() < 640) {
+                   pageResponse({
+                        selectors: '.channel-contain',     //模块的类名，使用class来控制页面上的模块(1个或多个)
+                        mode : 'auto',     // auto || contain || cover 
+                        width : '1180',      //输入页面的宽度，只支持输入数值，默认宽度为320px
+                        height : '80'      //输入页面的高度，只支持输入数值，默认高度为504px
+                    })
+                }
             },
         });
     });
