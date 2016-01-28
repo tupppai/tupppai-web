0c9fe42f2b0ca2b4a8e884c9971018acd9930644
FFF <104260033@qq.com>
2016-01-29 00:23:33 +0800

    签名

--- app/Http/Middleware/AppSignVerifyMiddleware.php
+++ app/Http/Middleware/AppSignVerifyMiddleware.php
@@ -8,20 +8,14 @@ class AppSignVerifyMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $method   = $request->method();
-        $version  = $request->get('v', null);
-        $verify   = $request->get('verify', null);
-
-        if ('GET' == $method) {
-            $args = $_GET; //or post
-        } else {
-            $args = $_POST; //or post
-        }
-
+        $version = $request->input('v', null);
+        $verify  = $request->input('verify', null);
+        $args    = $request->input();
         if ($version == 2 && $verify !== null) {
             unset($args['verify']);
+            unset($args['v']);
             if (!sign($args, $verify)) {
-                error('ERROR_SIGN_FAIL','签名错误');
+                error('ERROR_SIGN_FAIL', '签名错误');
             }
         }
 
