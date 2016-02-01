221108f1e9e6501069de5d27dc48793e0ea0e5d5
tupppai <tupppai@tupppaideMac-mini.local>
2016-02-01 15:31:29 +0800

    js

--- public/sharecourse/js/sharecourse.js
+++ public/sharecourse/js/sharecourse.js
@@ -15,12 +15,18 @@ $(function() {
 			if(isStar) {
 				$(".middle-v-icon").removeClass("blo");
 			};
+			for (var i = 0; i < data.data.ask_uploads.length; i++) {
+				var newContainer = $(".course-big-pic").eq(0).clone();
+				$(".course-big-pic a img").attr("src", data.data.ask_uploads[i].image_url)
+			};
+			newContainer.appendTo($(".course-explain"));
+			
+
 			$(".course-head-left span img").attr("src", data.data.avatar);
 			$(".course-name").html(data.data.nickname);
 			$(".course-name-time").html(time);
 			$(".explain-header").html(data.data.title);
 			$(".explain-p").html(data.data.description);
-			$(".course-big-pic a img").attr("src", data.data.image_url);
 			$(".like-num").html(data.data.up_count);
 			$(".see-num span").html(data.data.click_count);
 		}
