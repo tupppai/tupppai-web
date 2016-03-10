7e5fe4a76e441bf886ef26ec8523b6a2bb4fb42b
skys215 <skys215@gmail.com>
2016-03-10 14:40:54 +0800

    Fixed bug, nickname cannot be null

--- app/Services/User.php
+++ app/Services/User.php
@@ -111,7 +111,7 @@ class User extends ServiceBase
 
         $user =self::addNewUser($username, $password, $nickname, $mobile, $location, $avatar, $sex );
         if( $type != 'mobile' ){
-            sUserLanding::addNewUserLanding($user->uid, $openid, $type);
+            sUserLanding::addNewUserLanding($user->uid, $openid, $nickname, $type);
         }
         $role = sUserRole::assignRole($user->uid, mUser::ROLE_NEWBIE );
         return $user;
