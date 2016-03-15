0a61a08b33e9cbe684c9e86689feedfcd1bef03d
Valentino <valentino@163.com>
2016-03-15 17:28:22 +0800

    updata

--- public/boys/src/app/app.js
+++ public/boys/src/app/app.js
@@ -2,7 +2,7 @@ define('app/app', [ 'marionette', ],
     function (marionette) {
         "use strict";
         var app  = new marionette.Application();
-debugger;
+
         app.addRegions({
             content: '#contentView',
         });
