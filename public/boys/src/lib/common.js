139081e1d304949d2d1e6451837e17e31ca46e6a
junqiang <whenjonny@gmail.com>
2016-03-16 16:09:12 +0800

    微信授权绑定‘

--- public/boys/src/lib/common.js
+++ public/boys/src/lib/common.js
@@ -95,8 +95,10 @@ function append(el, item, options) {
 
 function parse(resp, xhr) { 
     if(resp.ret == 2) {
-        //图派男神活动授权
-        location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxa0b2dda705508552&redirect_uri=http://film.tupppai.com/wechat&response_type=code&scope=snsapi_userinfo#wechat_redirect';
+        var appid = resp.data.wx_appid;
+        var host = location.host;
+        var rd = encodeURIComponent('?hash='+location.pathname);
+        location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+appid+'&redirect_uri=http://'+host+'/wechat'+rd+'&response_type=code&scope=snsapi_userinfo#wechat_redirect';
     }
     if(resp.ret == 0 && resp.code == 1  ) {
 
