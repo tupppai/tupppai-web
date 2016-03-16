5c040342b97952d5a2810b4def652dade3432656
tupppai <tupppai@tupppaideMac-mini.local>
2016-03-16 13:57:06 +0800

    src

--- public/boys/src/app/views/getavatar/GetAvatarView.js
+++ public/boys/src/app/views/getavatar/GetAvatarView.js
@@ -6,6 +6,16 @@ define(['app/views/base', 'tpl!app/views/getavatar/GetAvatarView.html'],
             tagName: 'div',
             className: '',
             template: template,
+            events: {
+            	"click .upload": "popHowto",
+            	"click .pop-howto": "popHowto",
+            },
+            popHowto : function(e) {
+            	$(".pop-howto").removeClass("none");
+            	if($(e.target).hasClass("pop-howto")) {
+            		$(".pop-howto").addClass("none");
+            	}
+            }
         });
     });
 
