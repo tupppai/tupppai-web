848ca99ed2d32fbfa20584da98e14184f24bf048
Valentino <valentino@163.com>
2016-03-16 16:22:33 +0800

    updata

--- public/boys/src/lib/common.js
+++ public/boys/src/lib/common.js
@@ -132,21 +132,23 @@ function wx_sign() {
         });
     });
 };
+
 //拍照或从手机相册中选图接口
 function wx_choose_image() {
-    debugger;
+    alert( 1 );
     wx.chooseImage({
         count: 1, // 默认9
         sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
         sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
         success: function (res) {
             var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-
+            alert( res );
             //微信上传图片接口
             wx.uploadImage({
                 localId: localIds, // 需要上传的图片的本地ID，由chooseImage接口获得
                 isShowProgressTips: 1, // 默认为1，显示进度提示
                 success: function (res) {
+                    alert( res );
                     var serverId = res.serverId; // 返回图片的服务器端ID
                 }
             });
