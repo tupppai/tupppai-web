23e577584b8d8b3a84a2e2aafd4fb47f75d8e699
tupppai <tupppai@tupppaideMac-mini.local>
2016-03-17 10:28:12 +0800

    router.js

--- public/boys/src/app/views/shareavatar/ShareAvatarView.js
+++ public/boys/src/app/views/shareavatar/ShareAvatarView.js
@@ -7,10 +7,15 @@ define(['app/views/base', 'tpl!app/views/shareavatar/ShareAvatarView.html'],
             className: '',
             template: template,
             events: {
-            	'click #uploadImage': 'uploadImage'
+                'click #uploadImage': 'uploadImage',
+            	'click .effect-list img': 'replaceAvatar',
             },
             uploadImage:function() {
             	wx_choose_image();
+            },
+            replaceAvatar: function(e) {
+                var src = $(e.currentTarget).attr("src");
+                $(".after").attr("src", src);
             }
         });
     });
