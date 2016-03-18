5f5df9a5c5972d9d286ce9eef9d538f2f479ca3e
Valentino <valentino@163.com>
2016-03-18 19:48:24 +0800

    updata

--- public/boys/src/app/models/user.js
+++ public/boys/src/app/models/user.js
@@ -4,18 +4,9 @@ define(['app/models/base'], function(Model) {
         defaults: {
  			code: 1,
  			data: {
-				result: {
-					oper_by: "1",
-					oper_time: 1458292687,
-					assign_user: null,
 					total_amount:'',
 					left_amount:'',
-					assign_status: "reject",
 					reason: "就是太帅了",
-					username: "billqiang",
-					nickname: "jq",
-				},
- 				desc: "",
  			},
         }
 	});
