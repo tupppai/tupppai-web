b456190ef14c631a02cd4540a3c84e147ef75418
tupppai <tupppai@tupppaideMac-mini.local>
2016-03-18 16:24:55 +0800

    boys

--- public/boys/src/app/views/obtainsuccess/ObtainSuccessView.js
+++ public/boys/src/app/views/obtainsuccess/ObtainSuccessView.js
@@ -6,5 +6,20 @@ define(['app/views/base', 'tpl!app/views/obtainsuccess/ObtainSuccessView.html'],
             tagName: 'div',
             className: '',
             template: template,
+            onShow: function() {
+                var url = location.href;
+                var tmp1 = url.split("#")[1]; //获取到？后面的
+
+                var boy_id = tmp1.split("/")[0]; //男神ID
+                var effect_id = tmp1.split("/")[1]; //效果ID
+
+                $(".avatar-name").eq(boy_id).removeClass("none").siblings(".avatar-name").addClass("none");  //谁的效果说明
+                $(".avatar-name").eq(boy_id).find("img").eq(effect_id).removeClass("none").siblings("img").addClass("none");  //谁的哪种效果说明
+
+                // $(".avatar-effect").eq(boy_id).removeClass("none").siblings(".avatar-effect").addClass("none");		//谁的效果
+                // $(".avatar-effect").eq(boy_id).find("img").eq(effect_id).removeClass("none").siblings("img").addClass("none");	//谁的哪种效果
+
+                $(".author").find("img").eq(effect_id).removeClass("none").siblings("img").addClass("none");  //效果作者
+            }
         });
     });
