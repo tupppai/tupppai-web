10d7eb25c95adc84635d4399f5aa1874581a9316
tupppai <tupppai@tupppaideMac-mini.local>
2016-03-18 19:14:07 +0800

    js

--- public/boys/src/app/views/obtainsuccess/ObtainSuccessView.js
+++ public/boys/src/app/views/obtainsuccess/ObtainSuccessView.js
@@ -7,11 +7,12 @@ define(['app/views/base', 'tpl!app/views/obtainsuccess/ObtainSuccessView.html'],
             className: '',
             template: template,
             onShow: function() {
-                var url = location.href;
-                var tmp1 = url.split("#")[1]; //获取到？后面的
+                // var url = location.href;
+                var dataDesc = $("body").attr("data-desc");
+                // var tmp1 = url.split("#")[1]; //获取到？后面的
 
-                var boy_id = tmp1.split("/")[0]; //男神ID
-                var effect_id = tmp1.split("/")[1]; //效果ID
+                var boy_id = dataDesc.split("-")[0]; //男神ID
+                var effect_id = dataDesc.split("-")[1]; //效果ID
 
                 $(".avatar-name").eq(boy_id).removeClass("none").siblings(".avatar-name").addClass("none");  //谁的效果说明
                 $(".avatar-name").eq(boy_id).find("img").eq(effect_id).removeClass("none").siblings("img").addClass("none");  //谁的哪种效果说明
