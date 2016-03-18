64b46ba56c587ea69e4c6bd91f1cc464a6aab9cf
Valentino <valentino@163.com>
2016-03-18 19:00:26 +0800

    updata

--- public/boys/src/app/app.js
+++ public/boys/src/app/app.js
@@ -10,7 +10,23 @@ define('app/app', [ 'app/models/user','marionette'], function (User, marionette)
         app.addInitializer(function (options){
             app.user.fetch({
                 success:function(data) {
-                    // $("body").attr("data-src", data.get('image'));
+                    var code = data.get('code');
+                    //求P成功 没有作品也没有被拒绝
+                    if(code == 1) {
+                        location.href = 'http://' + location.hostname + '/boys/obtainsuccess/obtainsuccess';
+                    } 
+                    //求P成功有作品
+                    if(code == 2) {
+                        location.href = 'http://' + location.hostname + '/boys/obtainsuccess/obtainsuccess';
+                    } 
+                    //求P被拒绝
+                    if(code == -1) {
+                        location.href = 'http://' + location.hostname + '/boys/uploadagain/uploadagain';
+                    } 
+                    if(code == -2 ) {
+                        location.href = 'http://' + location.hostname + '/boys/index/index';
+                    }
+                    $("body").attr("data-user", data.get('left_amount'));
                     // $("body").attr("data-rand", data.get('rand'));
                     // $("body").attr("data-name", data.get('designer_name'));
                 }
