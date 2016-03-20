e4bb7b397576b608f58fe339e79ad3c64dffd594
Valentino <valentino@163.com>
2016-03-20 20:58:04 +0800

    updata

--- public/boys/src/app/controllers/getavatar/getavatar.js
+++ public/boys/src/app/controllers/getavatar/getavatar.js
@@ -2,6 +2,7 @@ define(['app/models/user', 'app/views/getavatar/GetAvatarView' ],
 		function (User, GetAvatarView) {
     "use strict";
     return function() {
+        alert( 1 ); 
         var user = new User;
         user.url = '/wxactgod/index'
         var view = new GetAvatarView({
