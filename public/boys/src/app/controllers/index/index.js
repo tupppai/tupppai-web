63126b6bb5dca1085b874229ba2c068740bc87fc
Valentino <valentino@163.com>
2016-03-21 13:27:19 +0800

    updata

--- public/boys/src/app/controllers/index/index.js
+++ public/boys/src/app/controllers/index/index.js
@@ -2,10 +2,10 @@ define(['app/models/user', 'app/views/index/IndexView' ],
 	function (User, indexView) {
     "use strict";
     return function() {
-    	var user = new User;
-        user.url = '/wxactgod/index'
+    	// var user = new User;
+        // user.url = '/wxactgod/index'
         var view = new indexView({
-        	model: user
+        	// model: user
         });
         window.app.content.show(view);
     };
