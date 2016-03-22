62558d9c6582995d5dc6130820b1facaf1d64c9f
104260033 <104260033@qq.com>
2016-03-22 14:58:46 +0800

    EasyWeChat

--- config/wechat.php
+++ config/wechat.php
@@ -3,7 +3,7 @@ return [
     'debug'  => true,
     'use_alias'    => env('WECHAT_USE_ALIAS', false),
     'app_id'       => env('MP_APPID', 'YourAppId'), // 必填
-    'secret'       => env('MP_SECRET', 'YourSecret'), // 必填
+    'secret'       => env('MP_APPSECRET', 'YourSecret'), // 必填
     'token'        => env('MP_TOKEN', 'YourToken'),  // 必填
     // 加密模式需要，其它模式不需要
     'encoding_key' => env('MP_AES_KEY', 'YourEncodingAESKey'),
