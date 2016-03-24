67c29ac6f3650a86780707700d4bc92648522a2b
skys215 <skys215@gmail.com>
2016-03-24 21:30:14 +0800

    Fixed bug, AskReplies doesn't exist

--- app/Services/Reply.php
+++ app/Services/Reply.php
@@ -75,7 +75,7 @@ class Reply extends ServiceBase
         else {
             // 如果非兼职，则更新求助的作品数量
             if($ask) {
-                cAskReplies::inc($ask->id, _uid());
+                cAskCounts::inc($ask->id, 'reply');
             }
         }
         if( sUser::isBlocked( $uid ) ){
@@ -573,11 +573,9 @@ class Reply extends ServiceBase
         //Ask uploads
         //todo: change to Reply->with()
         $data['ask_uploads'] = [];
-        $data['reply_count'] = 0;
         if( $reply->ask_id ){
             $ask = sAsk::getAskById($reply->ask_id);
             $data['ask_uploads']    = sAsk::getAskUploads($ask->upload_ids, $width);
-            $data['reply_count']    = cAskReplies::get($ask->id, _uid());
         }
 
         cReplyCounts::inc($reply->id, 'click');
