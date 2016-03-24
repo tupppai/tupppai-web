21413d91b37526d967383067fca6e387b9d8030f
ZhaZhengRefn <512557403@qq.com>
2016-03-24 20:39:49 +0800

    workDetail first version

---
+++ public/tasks/src/app/views/detail/workDetail.js
@@ -0,0 +1,10 @@
+define(['app/views/base', 'tpl!app/views/detail/workDetail.html'],
+    function (View, template) {
+        "use strict";
+        
+        return View.extend({
+            tagName: 'div',
+            className: '',
+            template: template,
+        });
+    });
