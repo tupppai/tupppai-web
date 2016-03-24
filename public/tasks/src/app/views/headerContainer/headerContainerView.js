9a3b5f6802afc877df9292b069242fb7432eb2fc
tupppai <tupppai@tupppaideMac-mini.local>
2016-03-24 11:16:24 +0800

    header

---
+++ public/tasks/src/app/views/headerContainer/headerContainerView.js
@@ -0,0 +1,10 @@
+define(['app/views/base', 'tpl!app/views/headerContainer/headerContainer.html'],
+    function (View, template) {
+        "use strict";
+        
+        return View.extend({
+            tagName: 'div',
+            className: '',
+            template: template,
+        });
+    });
