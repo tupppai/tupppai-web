619ea27fb5a9c8c58e62b4c10241133033651846
ZhaZhengRefn <512557403@qq.com>
2016-03-28 16:19:05 +0800

    homepageRequest completed

--- public/tasks/src/app/views/homepageRequest/homepageRequestView.js
+++ public/tasks/src/app/views/homepageRequest/homepageRequestView.js
@@ -12,6 +12,26 @@ define(['app/views/base', 'tpl!app/views/homepageRequest/homepageRequest.html'],
             switchHomePage: function(e) {
             	$(e.currentTarget).removeClass("unactive")
             			.siblings().addClass("unactive");
+                /*animation of arrow & switch homepage-content*/
+                var targetClass = $(e.currentTarget).attr("class");
+                if (targetClass.indexOf("myRequest")!==-1){
+                    $(".icon-toggle-arrow").animate({
+                        left:"265px",
+                    },500);
+                    $(".content-myRequest").removeClass("hide").siblings().addClass("hide");
+                }
+                if (targetClass.indexOf("myOffer")!==-1){
+                    $(".icon-toggle-arrow").animate({
+                        left:"475px",
+                    },500);
+                    $(".content-myOffer").removeClass("hide").siblings().addClass("hide");
+                }
+                if (targetClass.indexOf("myWork")!==-1){
+                    $(".icon-toggle-arrow").animate({
+                        left:"680px",
+                    },500);
+                    $(".content-myWork").removeClass("hide").siblings().addClass("hide");
+                }
             },
         });
     });
