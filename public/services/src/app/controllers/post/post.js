0114a9f692789d6fb000a0c0f3f0f6bd3ede8b01
ZhaZhengRefn <512557403@qq.com>
2016-03-30 21:11:39 +0800

    window-fix

---
+++ public/services/src/app/controllers/post/post.js
@@ -0,0 +1,8 @@
+define([ 'app/views/post/postView' ], function (index) {
+    "use strict";
+    return function() {
+    	
+        var view = new index();
+        window.app.content.show(view);
+    };
+});
---
+++ public/services/src/app/views/post/postView.js
@@ -0,0 +1,12 @@
+define(['app/views/base', 'tpl!app/views/post/post.html'],
+    function (View, template) {
+        "use strict";
+        
+        return View.extend({
+            tagName: 'div',
+            className: '',
+            template: template,
+        });
+    });
+
+
