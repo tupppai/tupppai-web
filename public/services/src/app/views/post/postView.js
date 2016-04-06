be6cd5a25aa1573b39f13e0246fc367035b8527a
Lingwei Zeng <w273177160@163.com>
2016-04-06 17:30:06 +0800

    common

--- public/services/src/app/views/post/postView.js
+++ public/services/src/app/views/post/postView.js
@@ -6,6 +6,22 @@ define(['app/views/base', 'tpl!app/views/post/post.html'],
             tagName: 'div',
             className: '',
             template: template,
+            events: {
+            	"click .commentOption": "replyPopup",
+            	"click .cancel": "replyPopupHide",
+            	"click .window-fix": "windowFix",
+            },
+            replyPopup: function(e) {
+            	$("#replyWindow").removeClass("hide")
+            },            
+            replyPopupHide: function(e) {
+            	$("#replyWindow").addClass("hide")
+            },            
+            windowFix: function(e) {
+            	if($(e.target).hasClass("window-fix")) {
+            		$("#replyWindow").addClass("hide")
+            	}
+            },
         });
     });
 
