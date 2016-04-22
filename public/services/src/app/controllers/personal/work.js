56127691f7193e686e72e80ef18335e7c86def25
zlwtp <w273177160@163.com>
2016-04-22 15:40:41 +0800

    controllers

--- public/services/src/app/controllers/personal/work.js
+++
@@ -1,8 +0,0 @@
-define([ 'app/views/personal/work/workView' ], function (workView) {
-    "use strict";
-    return function() {
-    	
-        var view = new workView();
-        window.app.content.show(view);
-    };
-});
