52c446bbf4e1f150b478e8cd7b627ea60a4ce33c
104260033 <104260033@qq.com>
2016-04-22 15:07:02 +0800

    临时修复

--- database/migrations/2016_04_013_1231205_thread_index_target_type_category_categories_.php
+++ database/migrations/2016_04_013_1231205_thread_index_target_type_category_categories_.php
@@ -13,6 +13,7 @@ class ThreadIndexTargetTypeCategoryCategories extends Migration
     public function up()
     {
         Schema::table('thread_categories', function( $table ){
+            $table->dropIndex(['target_type','category_id']);
             //todo in好像会中断,需要优化下
             $table->index(['target_type','category_id']);
         });
--- database/migrations/2016_04_07_1231205_thread_index_categories_.php
+++ database/migrations/2016_04_07_1231205_thread_index_categories_.php
@@ -13,6 +13,8 @@ class ThreadIndexCategories extends Migration
     public function up()
     {
         Schema::table('thread_categories', function( $table ){
+            $table->dropIndex(['status','target_type','category_id']);
+            $table->dropIndex('target_id');
             //todo in好像会中断,需要优化下
            $table->index(['status','target_type','category_id']);
            $table->index('target_id');
