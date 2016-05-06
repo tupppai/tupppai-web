e9cc97e6b18482ca579d6ee0e05af9838062b19b
Kuiki <911yinhui911@163.com>
2016-05-06 16:26:31 +0800

    sAsk添加抓出所有零回复的代码，用WhereNotExist实现

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -1,6 +1,13 @@
 <?php namespace App\Http\Controllers\Admin;
 
+use App\Services\Ask as sAsk;
+
 class TaskController extends ControllerBase
 {
-
+    public function indexAction($type = 0)
+    {
+        $queue = sAsk::waitingQueue();
+        // dd($queue);
+        echo count($queue);
+    }
 }
