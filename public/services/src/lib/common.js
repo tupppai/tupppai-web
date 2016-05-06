730be7a2746cad6b5d81690fbf3b2b8b5de1a4f7
zlwtp <w273177160@163.com>
2016-05-06 18:45:24 +0800

    weixin

--- public/services/src/lib/common.js
+++ public/services/src/lib/common.js
@@ -143,9 +143,10 @@ function is_from_wechat() {
         return false;
     }
 }
+var wx = '';
 function wx_sign() {
     var url = location.href.replace(location.hash, '');
-    $.post('sign', {url: url}, function(data) {
+    $.post('/sign', {url: url}, function(data) {
         wx = require('wx');
         wx.config({
             debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
