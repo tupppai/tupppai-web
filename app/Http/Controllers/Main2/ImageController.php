716b6b65fe490d8dc97b42b8850b786530e8b01b
Kuiki <911yinhui911@163.com>
2016-05-09 16:25:29 +0800

    完成图片上传接口（wxsdk上传后转存七牛）
    从boys复制过来的，待测试

---
+++ app/Http/Controllers/Main2/ImageController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Http\Controllers\Main2;
+
+use App\Services\Wx as sWX;
+class ImageController extends ControllerBase
+{
+	public function upload()
+	{
+
+        $media_id = $this->get('media_id', 'string', 0);
+        $upload_ids = sWX::getUploadId( $media_id );
+        if( is_null($upload_ids)){
+            return error('WRONG_ARGUMENTS', 'token获取失败');
+        }
+        if( $upload_ids === 0 ){
+            return error('WRONG_ARGUMENTS', '获取图片失败');
+        }
+        if( $upload_ids === -1 ){
+            return error('WRONG_ARGUMENTS', '保存图片失败');
+        }
+        $upload = sUpload::getUploadById($upload_ids);
+        if(!$upload) {
+            return error('EMPTY_UPLOAD_ID');
+        }
+        return $this->output([
+            'result' => 'ok',
+            'id' => $ask->id,
+            'ask_id' => $ask->id,
+            'savename' => $upload->savename,
+            'pathname' => $upload->pathname
+        ]);
+	}
+}
