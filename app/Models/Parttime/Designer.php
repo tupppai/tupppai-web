fdd4ef4c27a3bed20056d1f6000f4bd1c9ba2fae
Kuiki <911yinhui911@163.com>
2016-05-09 17:00:15 +0800

    对designer model添加基础find方法

--- app/Models/Parttime/Designer.php
+++ app/Models/Parttime/Designer.php
@@ -3,5 +3,24 @@ namespace App\Models\Parttime;
 
 class Designer extends ModelBase
 {
+    /**
+     * 通过id获取设计师
+     */
+    public function get_designer_by_id($designer_id) {
+        $designer = self::find($designer_id);
 
+        return $designer;
+    }
+
+    /**
+     * 通过id集合获取设计师
+     */
+    public function get_designer_by_ids($designer_ids) {
+        #$designers = self::whereRaw(" FIND_IN_SET (id ,$designer_ids)")
+        #->get();
+        $designers = self::whereIn('id', $designer_ids)
+            ->get();
+
+        return $designers;
+    }
 }
