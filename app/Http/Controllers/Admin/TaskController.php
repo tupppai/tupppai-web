e805794b40609de7683a66b52effff16a0d8b4db
Kuiki <911yinhui911@163.com>
2016-05-11 18:20:43 +0800

    给设计师分配兼职任务的算法初步完成（待完善）

--- app/Http/Controllers/Admin/TaskController.php
+++ app/Http/Controllers/Admin/TaskController.php
@@ -6,9 +6,34 @@ use App\Services\Parttime\Designer as sDesigner;
 
 class TaskController extends ControllerBase {
 	public function indexAction() {
+		//需要分配给多少个兼职（这个和需求值相关）的计算阈值
+		$threshold = 400;
 		$waitingQueue = sAsk::waitingQueue();
 		$designersQueue = sDesigner::abilityQueue();
+		foreach ($waitingQueue as $waiting) {
+			//若设计师队列已空，则无设计师可用，跳出循环
+			if (empty($designersQueue)) {
+				break;
+			}
+			var_dump($waiting['id']);
+			//计算当前任务应当分配给几位设计师
+			$needDesigners = (int) ceil($waiting['priority'] / $threshold);
+			foreach ($designersQueue as $key => &$designer) {
+				//TODO:检查该设计师是否已接过当前任务，若接过并取消就不要再分配给他
+				//开始分配任务
+				echo '给设计师' . $designer['uid'] . '分配了任务' . $waiting['id'] . ',该设计狮剩余可接任务数为' . $designer['avaliable_tasks'] . '<br>';
+				//减少当前设计师可接任务数，若已为0，则从队列中删除当前设计师
+				$designer['avaliable_tasks']--;
+				if ($designer['avaliable_tasks'] <= 0) {
+					unset($designersQueue[$key]);
+				}
+				//减少当前任务所需分配的设计师数，若当前任务需要分配的设计师数以达成，则跳出循环
+				$needDesigners--;
+				if ($needDesigners <= 0) {
+					break;
+				}
+			}
+		}
 		var_dump($designersQueue);
-		echo count($waitingQueue);
 	}
 }
