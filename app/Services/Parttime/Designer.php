004682c5d907ecd8ad3a5321afa9f48794cf51a5
Kuiki <911yinhui911@163.com>
2016-05-12 14:35:07 +0800

    修改assignments表的状态含义

--- app/Services/Parttime/Designer.php
+++ app/Services/Parttime/Designer.php
@@ -23,7 +23,7 @@ class Designer extends ServiceBase {
 		$designerIds = array_column($aDesigners, 'uid');
 		mAssignment::select('assigned_to as uid', DB::raw('count(id) as count'))
 			->whereIn('assigned_to', $designerIds)
-			->where('status', 1)
+			->whereIn('status', [1, 2])
 			->groupBy('assigned_to')
 			->chunk(10000, function ($assignments) use (&$aDesigners) {
 				foreach ($assignments as $assignment) {
@@ -38,7 +38,7 @@ class Designer extends ServiceBase {
 		mAssignment::select('assigned_to as uid', 'status', 'create_time')
 			->whereIn('assigned_to', $designerIds)
 			->where('create_time', '>', $Deadline30)
-			->whereIn('status', [2, 3])
+			->whereIn('status', [3, 4])
 		// ->groupBy('assigned_to')
 			->chunk(10000, function ($assignments) use (&$aDesigners, $Deadline7, $Deadline3) {
 				foreach ($assignments as $assignment) {
