080436c636cd76256d8999316b28feb39e3a2090
Kuiki <911yinhui911@163.com>
2016-05-12 12:03:23 +0800

    给assignments添加upload_time与grade，计算设计师近期任务时兼容assignment的status为3的情况

---
+++ database/migrations/2016_05_12_114745_new_column_assignments.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class NewColumnAssignments extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::connection('db_parttime')->table('assignments', function (Blueprint $table) {
+			$table->integer('upload_time')->unsigned()->default(0); //上传时间，未上传时为0
+			$table->tinyInteger('grade')->unsigned()->default(0); //若已打分，则status应当为3
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::connection('db_parttime')->table('assignments', function (Blueprint $table) {
+			$table->dropColumn('upload_time');
+			$table->dropColumn('grade');
+		});
+	}
+}
