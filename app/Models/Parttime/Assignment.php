b812a39d51d145eac31e106599c4090c5df649b8
skys215 <skys215@gmail.com>
2016-05-18 16:43:55 +0800

    Replay 7e31c0b

--- app/Models/Parttime/Assignment.php
+++ app/Models/Parttime/Assignment.php
@@ -5,9 +5,16 @@ use App\Models\ModelBase;
 class Assignment extends ModelBase {
 	protected $connection = 'db_parttime';
 	/**
+	 * 通过用户id获取任务集合
+	 */
+	public function get_assignments_by_uid($uid) {
+		$assignments = self::where('assigned_to',$uid)->get();
+		return $assignments;
+	}
+	/**
 	 * 通过id获取任务
 	 */
-	public function getAssignmentById($assignment_id) {
+	public function get_assignment_by_id($assignment_id) {
 		$assignment = self::find($assignment_id);
 
 		return $assignment;
@@ -16,7 +23,7 @@ class Assignment extends ModelBase {
 	/**
 	 * 通过id集合获取任务
 	 */
-	public function getAssignmentByIds($assignment_ids) {
+	public function get_assignment_by_ids($assignment_ids) {
 		#$assignments = self::whereRaw(" FIND_IN_SET (id ,$assignment_ids)")
 		#->get();
 		$assignments = self::whereIn('id', $assignment_ids)
