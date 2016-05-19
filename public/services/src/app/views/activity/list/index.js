1ff4cbd38c7bd13954a7d961cbdbab24ddfb913b
tupppai <apple@AppledeMac-mini.local>
2016-05-19 16:13:04 +0800

    activity

--- public/services/src/app/views/activity/list/index.js
+++ public/services/src/app/views/activity/list/index.js
@@ -1,18 +1,18 @@
-define(['app/views/activity/index/indexView', 'lib/component/asyncList'], function (indexView) {
+define(['app/views/activity/works/worksView', 'lib/component/asyncList'], function (worksView) {
     "use strict";
     
     return window.app.list.extend({
         tagName: 'div',
         className: 'hot-pageSection clearfix grid',
-    	childView: indexView,
+    	childView: worksView,
         onShow: function() {
             title('活动');
             $(".menuPs").removeClass("hide");
-            // this.$el.asynclist({
-            //     root: this,
-            //     renderMasonry: true,
-            //     itemSelector: 'loading',
-            // });
+            this.$el.asynclist({
+                root: this,
+                renderMasonry: true,
+                itemSelector: 'loading',
+            });
         }
     });
 });
