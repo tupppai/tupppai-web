fb60313c19bd410922ff179e61b610fa9d615b60
skys215 <skys215@gmail.com>
2016-06-14 14:47:55 +0800

    Added batch comment on verify/thread

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -1,11 +1,13 @@
 <?php namespace App\Http\Controllers\Admin;
 
 use App\Models\Comment as mComment;
+use App\Models\Puppet as mPuppet;
 use App\Models\User as mUser;
 
 use App\Services\Comment as sComment;
 use App\Services\Reply as sReply;
 use App\Services\User as sUser;
+use App\Services\Puppet as sPuppet;
 use App\Services\CommentStock as sCommentStock;
 
 use App\Jobs\PuppetComment;
@@ -141,27 +143,49 @@ class CommentController extends ControllerBase
         $comment_id = $this->post( 'commentId', 'int', 0 );
         $target_type = $this->post( 'target_type', 'int' );
         $comment_delay = $this->post( 'delay', 'int' );
+        $comment_mode = $this->post( 'comment_mode', 'string', 'single_comment' );
+        $comment_amount = $this->post( 'comment_amount', 'int', 1 );
 
-        if( $comment_id ){
-            $cmntStock = sCommentStock::getCommentByStockId( $this->_uid, $comment_id );
-            $content = $cmntStock->content;
+        if( $comment_mode == 'single_comment' ){
+            if( $save == 'on' ){
+                $cmntStock = sCommentStock::addComments( $this->_uid, [$content] );
+                $comment_id = $cmntStock->id;
+            }
+
+            if( $comment_id ){
+                $cmntStock = sCommentStock::getCommentByStockId( $this->_uid, $comment_id );
+                $content = $cmntStock->content;
+            }
+            $comment_uids = [$user_id];
+            $contents  = [$content];
+            $comment_ids = [0];
         }
-        if( !$content ){
-            return error('EMPTY_COMMENT');
+        else{
+            $comments = sCommentStock::getComments( $this->_uid )->toArray();
+            $roles = [ mPuppet::ROLE_CRITIC ];
+            $puppets = sPuppet::getPuppets( $this->_uid, $roles );
+            $comment_amount = mt_rand( $comment_amount-3, $comment_amount+3 );
+            $comment_amount = min( count($puppets), count($comments), $comment_amount );
+
+            shuffle( $puppets );
+            $comment_puppets = array_slice( $puppets, 0, $comment_amount );
+            $comment_uids = array_column( $comment_puppets, 'uid' );
+
+            shuffle( $comments );
+            $comment_contents = array_slice( $comments, 0, $comment_amount );
+            $contents = array_column( $comment_contents, 'content' );
+            $comment_ids = array_column( $comment_contents, 'id' );
         }
 
         $comment_delay = Carbon::now()->addSeconds($comment_delay);
-        Queue::later( $comment_delay, new PuppetComment( $user_id, $content, $target_type, $target_id ));
-        //$comment = sComment::addNewComment( $user_id, $content, $target_type, $target_id );
 
-        if( $save == 'on' ){
-            $cmntStock = sCommentStock::addComments( $this->_uid, [$content] );
-            $comment_id = $cmntStock->id;
-        }
-        if( $comment_id ){
+        foreach( $comment_uids as $key => $uid ){
+            $content = $contents[$key];
+            Queue::later( $comment_delay, new PuppetComment( $uid, $content, $target_type, $target_id ));
+
+            $comment_id = $comment_ids[$key];
             $cmntStock = sCommentStock::usedComment( $comment_id );
         }
-
 
         return $this->output_json( ['result'=>'ok'] );
     }
