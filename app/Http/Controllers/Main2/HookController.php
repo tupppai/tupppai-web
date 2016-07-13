434db4cc562f95642e9ad7ea36f9c1ed4828b945
skys215 <skys215@gmail.com>
2016-07-13 17:51:31 +0800

    Added wxmsg push for receiving reward

--- app/Http/Controllers/Main2/HookController.php
+++ app/Http/Controllers/Main2/HookController.php
@@ -5,11 +5,17 @@ use App\Trades\User as tUser;
 use App\Trades\Transaction as tTransaction;
 
 use App\Services\Ask as sAsk;
+use App\Services\User as sUser;
 use App\Services\Comment as sComment;
 use App\Services\Reward as sReward;
 use App\Services\Reply as sReply;
+use App\Services\WXMsg as sWXMsg;
+use App\Services\UserLanding as sUserLanding;
 
 use App\Models\Reward as mReward;
+use App\Models\UserLanding as mUserLanding;
+
+use App\Jobs\Push as jPush;
 
 use Log;
 use DB;
@@ -61,18 +67,39 @@ class HookController extends ControllerBase{
 			            if(!$reward) {
 			                return error('TRADE_PAY_ERROR', '打赏失败');
 			            }
+                        $author_uid = 0;
+                        $jumpUrl = '';
+
 						if($trade->attach->target_type == mReward::TYPE_ASK ) {
 		                    $ask = sAsk::getAskById($trade->attach->target_id);
-		                    tUser::pay($trade->uid, $ask->uid, $amount, '打赏');
-		                }
-		                else if( $trade->attach->target_type == mReward::TYPE_REPLY ){
-							//支付打赏的回调逻辑
-							$reply = sReply::getReplyById($trade->attach->target_id);
-		                    tUser::pay($trade->uid, $reply->uid, $amount, '打赏');
-						}
+                            $author_uid = $ask->uid;
+                            $jumpUrl = '/services/?/#detail/works/'.$ask->id;
+                        }
+                        else if( $trade->attach->target_type == mReward::TYPE_REPLY ){
+                            //支付打赏的回调逻辑
+                            $reply = sReply::getReplyById($trade->attach->target_id);
+                            $author_uid = $reply->uid;
+                            $jumpUrl = '/services/?/#detail/detail/2/'.$reply->id;
+                        }
+                        tUser::pay($trade->uid, $author_uid, $amount, '打赏');
+
                         $commentHead = '打赏了'.number_format($amount/100,2).'元，并说：';
 						//留言 评论
 				        $comment = sComment::addNewComment($trade->uid, $commentHead.$trade->attach->comment, $trade->attach->target_type, $trade->attach->target_id);
+                        $guest = sUser::getUserByUid( $trade->uid );
+                        $authorUserLanding = sUserLanding::getUserLandingByUid( $author_uid, mUserLanding::TYPE_WEIXIN_MP );
+                        if( $authorUserLanding ){
+                            // 打赏成功发送微信模板消息
+                            $tplVars = [
+                                'first'=>'禀告大神：小热粉“Carina'.$author->username.'”给您进贡了'.$amount.'元，赶紧戳这里临幸一下他吧~',
+                                'keyword1' => number_format($amount/100,2).'元',
+                                'keyword2' => date('Y-m-d', $trade->trade_finish_time),
+                                'keyword3' => '图派打赏',
+                                'keyword4' => $trade->attach->comment,
+                                'remark' => '贡品已打到您的图派账户，请到图派个人主页查看小金库吧~~被赞，评论，分享作品，打赏等都会增加您作品的曝光度，帮你上13区（热门），接受更多人的膜拜哦~~'
+                            ];
+                            sWXMsg::sendMsg( sWXMsg::TPL_ID_HAS_NEW_REWARD, $tplVars, [$authorUserLanding->openid], $jumpUrl );
+                        }
 				    }
                 }
                 else {
